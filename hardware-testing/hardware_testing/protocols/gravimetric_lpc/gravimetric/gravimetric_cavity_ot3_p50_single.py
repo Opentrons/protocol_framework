3893073f9c0326c8737ff5b6fa82f18e6db292db
Ryan howard <ryan.howard@opentrons.com>
2024-07-18 16:37:35 -0400

    fixups for 20ul tips

--- hardware-testing/hardware_testing/protocols/gravimetric_lpc/gravimetric/gravimetric_cavity_ot3_p50_single.py
+++ hardware-testing/hardware_testing/protocols/gravimetric_lpc/gravimetric/gravimetric_cavity_ot3_p50_single.py
@@ -1,13 +1,14 @@
 """Gravimetric OT3."""
 from opentrons.protocol_api import ProtocolContext
+from opentrons.protocol_api._types import OffDeckType
 
-metadata = {"protocolName": "gravimetric-ot3-p50-single"}
+metadata = {"protocolName": "gravimetric-cavity-ot3-p50-single"}
 requirements = {"robotType": "Flex", "apiLevel": "2.15"}
 
 SLOT_SCALE = 4
 SLOTS_TIPRACK = {
-    50: [3],
-    20: [6],
+    20: [2, 3, 5, 6, 7, 8, 9, 10],
+    50: [2, 3, 5, 6, 7, 8, 9, 10]
 }
 LABWARE_ON_SCALE = "radwag_pipette_calibration_vial"
 
@@ -26,3 +27,8 @@ def run(ctx: ProtocolContext) -> None:
         pipette.aspirate(10, vial["A1"].top())
         pipette.dispense(10, vial["A1"].top())
         pipette.drop_tip(home_after=False)
+        ctx.move_labware(
+                rack,
+                new_location=OffDeckType.OFF_DECK,
+                use_gripper=False,
+            )
