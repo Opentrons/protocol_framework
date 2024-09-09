74c4e0d5c4ab891478fd0d92a9eb1177dee0b27e
Laura Cox <laura@opentrons.com>
2024-09-09 17:41:50 +0300

    feat: add robot core

--- api/src/opentrons/protocol_api/core/legacy/legacy_protocol_core.py
+++ api/src/opentrons/protocol_api/core/legacy/legacy_protocol_core.py
@@ -266,6 +266,10 @@ class LegacyProtocolCore(
     ) -> LegacyLabwareCore:
         """Load an adapter using its identifying parameters"""
         raise APIVersionError(api_element="Loading adapter")
+    
+    def load_robot(self) -> None:
+        """Load an adapter using its identifying parameters"""
+        raise APIVersionError(api_element="Loading robot")
 
     def load_robot(self) -> None:  # type: ignore
         """Load an adapter using its identifying parameters"""
