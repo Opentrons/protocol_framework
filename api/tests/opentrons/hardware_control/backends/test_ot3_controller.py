7f98b782d5f7f7853ec37d53bfa7ed66c30ababb
Laura Cox <laura@opentrons.com>
2024-10-17 15:43:14 +0300

    fix: correctly handle Q motor moves in the  function and add a test

--- api/tests/opentrons/hardware_control/backends/test_ot3_controller.py
+++ api/tests/opentrons/hardware_control/backends/test_ot3_controller.py
@@ -197,11 +197,12 @@ def mock_send_stop_threshold() -> Iterator[mock.AsyncMock]:
 
 @pytest.fixture
 def mock_move_group_run() -> Iterator[mock.AsyncMock]:
+
     with mock.patch(
         "opentrons.hardware_control.backends.ot3controller.MoveGroupRunner.run",
         autospec=True,
     ) as mock_mgr_run:
-        mock_mgr_run.return_value = {}
+        mock_mgr_run.side_effect = {}
         yield mock_mgr_run
 
 
