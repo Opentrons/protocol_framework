22537d273e3dec128c29efa20af9da454d32a719
jbleon95 <jeremybenleon@gmail.com>
2024-10-28 14:16:20 -0400

    python classes for different settings

--- api/src/opentrons/protocol_api/_liquid.py
+++ api/src/opentrons/protocol_api/_liquid.py
@@ -1,14 +1,23 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
-from typing import Optional, Sequence
+from typing import Optional, Sequence, Dict
 
 from opentrons_shared_data.liquid_classes.liquid_class_definition import (
     LiquidClassSchemaV1,
-    AspirateProperties,
-    SingleDispenseProperties,
-    MultiDispenseProperties,
+    AspirateProperties as SharedDataAspirateProperties,
+    SingleDispenseProperties as SharedDataSingleDispenseProperties,
+    MultiDispenseProperties as SharedDataMultiDispenseProperties,
     ByPipetteSetting,
     ByTipTypeSetting,
+    BlowoutLocation,
+    PositionReference,
+    Coordinate,
 )
+
+# TODO replace this with a class that can extrapolate given volumes to the correct float,
+#   also figure out how we want people to be able to set this
+LiquidHandlingPropertyByVolume = Dict[str, float]
 
 
 @dataclass(frozen=True)
@@ -29,26 +38,425 @@ class Liquid:
     display_color: Optional[str]
 
 
+class DelayProperties:
+    def __init__(self, enable: bool, duration: Optional[float]) -> None:
+        self._enabled = enable
+        self._duration = duration
+
+    @property
+    def enabled(self) -> bool:
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, enable: bool) -> None:
+        # TODO insert bool validation here
+        if enable and self._duration is None:
+            raise ValueError("duration must be set before enabling delay.")
+        self._enabled = enable
+
+    @property
+    def duration(self) -> Optional[float]:
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration: float) -> None:
+        # TODO insert positive float validation here
+        self._duration = new_duration
+
+
+class TouchTipProperties:
+    def __init__(
+        self,
+        enable: bool,
+        z_offset: Optional[float],
+        mm_to_edge: Optional[float],
+        speed: Optional[float],
+    ) -> None:
+        self._enabled = enable
+        self._z_offset = z_offset
+        self._mm_to_edge = mm_to_edge
+        self._speed = speed
+
+    @property
+    def enabled(self) -> bool:
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, enable: bool) -> None:
+        # TODO insert bool validation here
+        if enable and (
+            self._z_offset is None or self._mm_to_edge is None or self._speed is None
+        ):
+            raise ValueError(
+                "z_offset, mm_to_edge and speed must be set before enabling touch tip."
+            )
+        self._enabled = enable
+
+    @property
+    def z_offset(self) -> Optional[float]:
+        return self._z_offset
+
+    @z_offset.setter
+    def z_offset(self, new_offset: float) -> None:
+        # TODO validation for float
+        self._z_offset = new_offset
+
+    @property
+    def mm_to_edge(self) -> Optional[float]:
+        return self._mm_to_edge
+
+    @mm_to_edge.setter
+    def mm_to_edge(self, new_mm: float) -> None:
+        # TODO validation for float
+        self._z_offset = new_mm
+
+    @property
+    def speed(self) -> Optional[float]:
+        return self._speed
+
+    @speed.setter
+    def speed(self, new_speed: float) -> None:
+        # TODO insert positive float validation here
+        self._speed = new_speed
+
+
+class MixProperties:
+    def __init__(
+        self, enable: bool, repetitions: Optional[int], volume: Optional[float]
+    ) -> None:
+        self._enabled = enable
+        self._repetitions = repetitions
+        self._volume = volume
+
+    @property
+    def enabled(self) -> bool:
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, enable: bool) -> None:
+        # TODO insert bool validation here
+        if enable and (self._repetitions is None or self._volume is None):
+            raise ValueError("repetitions and volume must be set before enabling mix.")
+        self._enabled = enable
+
+    @property
+    def repetitions(self) -> Optional[int]:
+        return self._repetitions
+
+    @repetitions.setter
+    def repetitions(self, new_repetitions: int) -> None:
+        # TODO validations for positive int
+        self._repetitions = new_repetitions
+
+    @property
+    def volume(self) -> Optional[float]:
+        return self._volume
+
+    @volume.setter
+    def volume(self, new_volume: float) -> None:
+        # TODO validations for volume float
+        self._volume = new_volume
+
+
+class BlowoutProperties:
+    def __init__(
+        self,
+        enable: bool,
+        location: Optional[BlowoutLocation],
+        flow_rate: Optional[float],
+    ) -> None:
+        self._enabled = enable
+        self._location = location
+        self._flow_rate = flow_rate
+
+    @property
+    def enabled(self) -> bool:
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, enable: bool) -> None:
+        # TODO insert bool validation here
+        if enable and (self._location is None or self._flow_rate is None):
+            raise ValueError(
+                "location and flow_rate must be set before enabling blowout."
+            )
+        self._enabled = enable
+
+    @property
+    def location(self) -> Optional[BlowoutLocation]:
+        return self._location
+
+    @location.setter
+    def location(self, new_location: str) -> None:
+        # TODO blowout location validation
+        self._location = BlowoutLocation(new_location)
+
+    @property
+    def flow_rate(self) -> Optional[float]:
+        return self._flow_rate
+
+    @flow_rate.setter
+    def flow_rate(self, new_flow_rate: float) -> None:
+        # TODO validations for positive float
+        self._flow_rate = new_flow_rate
+
+
+class Submerge:
+    def __init__(
+        self,
+        position_reference: PositionReference,
+        offset: Coordinate,
+        speed: float,
+        delay: DelayProperties,
+    ) -> None:
+        self._position_reference = position_reference
+        self._offset = offset
+        self._speed = speed
+        self._delay = delay
+
+    @property
+    def position_reference(self) -> PositionReference:
+        return self._position_reference
+
+    @position_reference.setter
+    def position_reference(self, new_position: str) -> None:
+        # TODO validation for position reference
+        self._position_reference = PositionReference(new_position)
+
+    @property
+    def offset(self) -> Coordinate:
+        return self._offset
+
+    @offset.setter
+    def offset(self, new_offset: Sequence[float]) -> None:
+        # TODO validate valid coordinates
+        self._offset = Coordinate(x=new_offset[0], y=new_offset[1], z=new_offset[2])
+
+    @property
+    def speed(self) -> float:
+        return self._speed
+
+    @speed.setter
+    def speed(self, new_speed: float) -> None:
+        # TODO insert positive float validation here
+        self._speed = new_speed
+
+    @property
+    def delay(self) -> DelayProperties:
+        return self._delay
+
+
+class RetractAspirate(Submerge):
+    def __init__(
+        self,
+        position_reference: PositionReference,
+        offset: Coordinate,
+        speed: float,
+        delay: DelayProperties,
+        air_gap_by_volume: LiquidHandlingPropertyByVolume,
+        touch_tip: TouchTipProperties,
+    ) -> None:
+        super().__init__(position_reference, offset, speed, delay)
+        self._air_gap_by_volume = air_gap_by_volume
+        self._touch_tip = touch_tip
+
+    @property
+    def air_gap_by_volume(self) -> LiquidHandlingPropertyByVolume:
+        return self._air_gap_by_volume
+
+    @property
+    def touch_tip(self) -> TouchTipProperties:
+        return self._touch_tip
+
+
+class RetractDispense(RetractAspirate):
+    def __init__(
+        self,
+        position_reference: PositionReference,
+        offset: Coordinate,
+        speed: float,
+        delay: DelayProperties,
+        air_gap_by_volume: LiquidHandlingPropertyByVolume,
+        touch_tip: TouchTipProperties,
+        blowout: BlowoutProperties,
+    ) -> None:
+        super().__init__(
+            position_reference, offset, speed, delay, air_gap_by_volume, touch_tip
+        )
+        self._blowout = blowout
+
+    @property
+    def blowout(self) -> BlowoutProperties:
+        return self._blowout
+
+
+class BaseLiquidHandlingProperties:
+    def __init__(
+        self,
+        submerge: Submerge,
+        position_reference: PositionReference,
+        offset: Coordinate,
+        flow_rate_by_volume: LiquidHandlingPropertyByVolume,
+        delay: DelayProperties,
+    ):
+        self._submerge = submerge
+        self._position_reference = position_reference
+        self._offset = offset
+        self._flow_rate_by_volume = flow_rate_by_volume
+        self._delay = delay
+
+    @property
+    def submerge(self) -> Submerge:
+        return self._submerge
+
+    @property
+    def position_reference(self) -> PositionReference:
+        return self._position_reference
+
+    @position_reference.setter
+    def position_reference(self, new_position: str) -> None:
+        # TODO validation for position reference
+        self._position_reference = PositionReference(new_position)
+
+    @property
+    def offset(self) -> Coordinate:
+        return self._offset
+
+    @offset.setter
+    def offset(self, new_offset: Sequence[float]) -> None:
+        # TODO validate valid coordinates
+        self._offset = Coordinate(x=new_offset[0], y=new_offset[1], z=new_offset[2])
+
+    @property
+    def flow_rate_by_volume(self) -> LiquidHandlingPropertyByVolume:
+        return self._flow_rate_by_volume
+
+    @property
+    def delay(self) -> DelayProperties:
+        return self._delay
+
+
+class AspirateProperties(BaseLiquidHandlingProperties):
+    def __init__(
+        self,
+        submerge: Submerge,
+        position_reference: PositionReference,
+        offset: Coordinate,
+        flow_rate_by_volume: LiquidHandlingPropertyByVolume,
+        delay: DelayProperties,
+        retract: RetractAspirate,
+        pre_wet: bool,
+        mix: MixProperties,
+    ):
+        super().__init__(
+            submerge, position_reference, offset, flow_rate_by_volume, delay
+        )
+        self._retract = retract
+        self._pre_wet = pre_wet
+        self._mix = mix
+
+    @property
+    def pre_wet(self) -> bool:
+        return self._pre_wet
+
+    @pre_wet.setter
+    def pre_wet(self, new_setting: bool) -> None:
+        # TODO boolean validation
+        self._pre_wet = new_setting
+
+    @property
+    def retract(self) -> RetractAspirate:
+        return self._retract
+
+    @property
+    def mix(self) -> MixProperties:
+        return self._mix
+
+
+class SingleDispenseProperties(BaseLiquidHandlingProperties):
+    def __init__(
+        self,
+        submerge: Submerge,
+        position_reference: PositionReference,
+        offset: Coordinate,
+        flow_rate_by_volume: LiquidHandlingPropertyByVolume,
+        delay: DelayProperties,
+        retract: RetractDispense,
+        mix: MixProperties,
+        push_out_by_volume: LiquidHandlingPropertyByVolume,
+    ):
+        super().__init__(
+            submerge, position_reference, offset, flow_rate_by_volume, delay
+        )
+        self._retract = retract
+        self._push_out_by_volume = push_out_by_volume
+        self._mix = mix
+
+    @property
+    def push_out_by_volume(self) -> LiquidHandlingPropertyByVolume:
+        return self._push_out_by_volume
+
+    @property
+    def retract(self) -> RetractDispense:
+        return self._retract
+
+    @property
+    def mix(self) -> MixProperties:
+        return self._mix
+
+
+class MultiDispenseProperties(BaseLiquidHandlingProperties):
+    def __init__(
+        self,
+        submerge: Submerge,
+        position_reference: PositionReference,
+        offset: Coordinate,
+        flow_rate_by_volume: LiquidHandlingPropertyByVolume,
+        delay: DelayProperties,
+        retract: RetractDispense,
+        conditioning_by_volume: LiquidHandlingPropertyByVolume,
+        disposal_by_volume: LiquidHandlingPropertyByVolume,
+    ):
+        super().__init__(
+            submerge, position_reference, offset, flow_rate_by_volume, delay
+        )
+        self._retract = retract
+        self._conditioning_by_volume = conditioning_by_volume
+        self._disposal_by_volume = disposal_by_volume
+
+    @property
+    def retract(self) -> RetractDispense:
+        return self._retract
+
+    @property
+    def conditioning_by_volume(self) -> LiquidHandlingPropertyByVolume:
+        return self._conditioning_by_volume
+
+    @property
+    def disposal_by_volume(self) -> LiquidHandlingPropertyByVolume:
+        return self._disposal_by_volume
+
+
 # TODO (spp, 2024-10-17): create PAPI-equivalent types for all the properties
 #  and have validation on value updates with user-facing error messages
 @dataclass
 class TransferProperties:
-    _aspirate: AspirateProperties
-    _dispense: SingleDispenseProperties
-    _multi_dispense: Optional[MultiDispenseProperties]
+    _aspirate: SharedDataAspirateProperties
+    _dispense: SharedDataSingleDispenseProperties
+    _multi_dispense: Optional[SharedDataMultiDispenseProperties]
 
     @property
-    def aspirate(self) -> AspirateProperties:
+    def aspirate(self) -> SharedDataAspirateProperties:
         """Aspirate properties."""
         return self._aspirate
 
     @property
-    def dispense(self) -> SingleDispenseProperties:
+    def dispense(self) -> SharedDataSingleDispenseProperties:
         """Single dispense properties."""
         return self._dispense
 
     @property
-    def multi_dispense(self) -> Optional[MultiDispenseProperties]:
+    def multi_dispense(self) -> Optional[SharedDataMultiDispenseProperties]:
         """Multi dispense properties."""
         return self._multi_dispense
 
