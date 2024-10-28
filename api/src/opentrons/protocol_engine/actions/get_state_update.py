d60e85d9d111109516b36019d7962847c9d1945d
Seth Foster <seth@opentrons.com>
2024-10-28 11:12:33 -0400

    really hard, i swear

--- api/src/opentrons/protocol_engine/actions/get_state_update.py
+++ api/src/opentrons/protocol_engine/actions/get_state_update.py
@@ -1,9 +1,12 @@
 # noqa: D100
-
+from __future__ import annotations
+from typing import TYPE_CHECKING
 
 from .actions import Action, SucceedCommandAction, FailCommandAction
 from ..commands.command import DefinedErrorData
-from ..state.update_types import StateUpdate
+
+if TYPE_CHECKING:
+    from ..state.update_types import StateUpdate
 
 
 def get_state_update(action: Action) -> StateUpdate | None:
