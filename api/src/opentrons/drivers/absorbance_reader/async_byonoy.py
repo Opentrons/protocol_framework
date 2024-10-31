9098e7932a3205329ed1fde9b4010b9e45e0305f
vegano1 <brayan.almonte@gmail.com>
2024-10-31 07:30:19 -0400

    fix(api): update the plate reader serial number parser to include BYO and OPT delims.

--- api/src/opentrons/drivers/absorbance_reader/async_byonoy.py
+++ api/src/opentrons/drivers/absorbance_reader/async_byonoy.py
@@ -24,7 +24,7 @@ from opentrons.hardware_control.modules.errors import AbsorbanceReaderDisconnect
 
 SN_PARSER = re.compile(r'ATTRS{serial}=="(?P<serial>.+?)"')
 VERSION_PARSER = re.compile(r"Absorbance (?P<version>V\d+\.\d+\.\d+)")
-SERIAL_PARSER = re.compile(r"(?P<serial>BYO[A-Z]{3}[0-9]{5})")
+SERIAL_PARSER = re.compile(r"(?P<serial>(OPT|BYO)[A-Z]{3}[0-9]{5})")
 
 
 class AsyncByonoy:
@@ -158,7 +158,7 @@ class AsyncByonoy:
         self._raise_if_error(err.name, f"Error getting device information: {err}")
         serial_match = SERIAL_PARSER.match(device_info.sn)
         version_match = VERSION_PARSER.match(device_info.version)
-        serial = serial_match["serial"] if serial_match else "BYOMAA00000"
+        serial = serial_match["serial"] if serial_match else "OPTMAA00000"
         version = version_match["version"].lower() if version_match else "v0.0.0"
         info = {
             "serial": serial,
