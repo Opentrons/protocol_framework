c19b205d29f6c8c9434bd1e3e0496b5aed68654a
Seth Foster <seth@opentrons.com>
2024-10-31 15:28:42 -0400

    refactor(api): track liquid in pipettes
    Today, we store the contents of a pipette as an optional float. If the
    value is None, then the pipette has an unknown amount of fluid in it and
    can't add or remove any; if the value is not None, then the pipette has
    that many ul of fluid in it. If we dispense, we dispense that much liquid
    into a well.
    
    This isn't good enough to accurately track liquid, because air gaps
    exist. We need to separately track how much liquid and how much air is
    in a pipette, and figure out how much of each we're dispensing when we
    dispense somewhere.
    
    To do that,
    - Swap out the aspirated_volume_by_id: Dict[str, Optional[float]] with
    pipette_contents_by_id: Dict[str, Optional[FluidStack]]
    - Swap out volume tracking from success/fail action based tracking in
    the stores to state update based setting from the commands
    - Swap out dispense(inPlace)? blindly adding the volume it dispensed to a target
    well with dispense(inPlace)? asking the store how much liquid it
    dispensed and adding that to the well
    
    This ends up being a tremendous amount of code because basically all of
    the tests for everything have to change, which is annoying. There's
    another step that should happen here where the state tests get
    simplified a lot but this was already huge and it's already very
    important that the underlying logic didn't change, so let's keep the
    diffs (relatively) small.

--- api/src/opentrons/protocol_engine/commands/drop_tip_in_place.py
+++ api/src/opentrons/protocol_engine/commands/drop_tip_in_place.py
@@ -65,7 +65,6 @@ class DropTipInPlaceImplementation(
     async def execute(self, params: DropTipInPlaceParams) -> _ExecuteReturn:
         """Drop a tip using the requested pipette."""
         state_update = update_types.StateUpdate()
-
         try:
             await self._tip_handler.drop_tip(
                 pipette_id=params.pipetteId, home_after=params.homeAfter
@@ -74,6 +73,10 @@ class DropTipInPlaceImplementation(
             state_update_if_false_positive = update_types.StateUpdate()
             state_update_if_false_positive.update_pipette_tip_state(
                 pipette_id=params.pipetteId, tip_geometry=None
+            )
+            state_update.set_fluid_unknown(pipette_id=params.pipetteId)
+            state_update_if_false_positive.set_fluid_unknown(
+                pipette_id=params.pipetteId
             )
             error = TipPhysicallyAttachedError(
                 id=self._model_utils.generate_id(),
@@ -92,6 +95,7 @@ class DropTipInPlaceImplementation(
                 state_update_if_false_positive=state_update_if_false_positive,
             )
         else:
+            state_update.set_fluid_unknown(pipette_id=params.pipetteId)
             state_update.update_pipette_tip_state(
                 pipette_id=params.pipetteId, tip_geometry=None
             )
