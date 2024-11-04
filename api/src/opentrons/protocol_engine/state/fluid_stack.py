737eb58d745b505687d4d04d3027caa7a46d7da2
Seth Foster <seth@opentrons.com>
2024-11-04 13:12:47 -0500

    stack terminology

--- api/src/opentrons/protocol_engine/state/fluid_stack.py
+++ api/src/opentrons/protocol_engine/state/fluid_stack.py
@@ -2,9 +2,9 @@
 
 Inside a pipette's tip, there can be a mix of kinds of fluids - here, "fluid" means "liquid" (i.e. a protocol-relevant
 working liquid that is aspirated or dispensed from wells) or "air" (i.e. because there was an air gap). Since sometimes
-you want air gaps in different places - below liquid to prevent dripping, above liquid to provide extra room to push the
-plunger - we need to support some notion of at least ordinal position of air and liquid, and we do so as a stack because
-that's physically relevant.
+you want air gaps in different places - physicall-ybelow liquid to prevent dripping, physically-above liquid to provide
+extra room to push the plunger - we need to support some notion of at least phsyical ordinal position of air and liquid,
+and we do so as a logical stack because that's physically relevant.
 """
 from logging import getLogger
 from numpy import isclose
@@ -16,8 +16,9 @@ _LOG = getLogger(__name__)
 class FluidStack:
     """A FluidStack data structure is a list of AspiratedFluids, with stack-style (last-in-first-out) ordering.
 
-    The front of the list is the top of the liquid stack and the back of the list is the bottom of the liquid stack. The
-    state is internal and the interaction surface is the methods. This is a mutating API.
+    The front of the list is the physical-top of the liquid stack (logical-bottom of the stack data structure)
+    and the back of the list is the physical-bottom of the liquid stack (logical-top of the stack data structure).
+    The state is internal and the interaction surface is the methods. This is a mutating API.
     """
 
     _FluidStack = list[AspiratedFluid]
@@ -34,8 +35,9 @@ class FluidStack:
     def add_fluid(self, new: AspiratedFluid) -> None:
         """Add fluid to a stack.
 
-        If the new fluid is of a different kind than what's on the bottom of the stack. add a new record; if the same
-        kind, add a new record to the end.
+        If the new fluid is of a different kind than what's on the physical-bottom of the stack, add a new record.
+        If the new fluid is of the same kind as what's on the physical-bottom of the stack, add the new volume to
+        the same record.
         """
         if len(self._fluid_stack) == 0 or self._fluid_stack[-1].kind != new.kind:
             # this is a new kind of fluid, append the record
@@ -62,7 +64,7 @@ class FluidStack:
         self._fluid_stack = removed
 
     def remove_fluid(self, volume: float) -> None:
-        """Remove a specific amount of fluid from the stack.
+        """Remove a specific amount of fluid from the physical-bottom of the stack.
 
         This will consume records that are wholly included in the provided volume and alter the remaining
         final records (if any) to decrement the amount of volume removed from it.
@@ -109,7 +111,7 @@ class FluidStack:
         return volume
 
     def liquid_part_of_dispense_volume(self, volume: float) -> float:
-        """Get the amount of liquid in the specified volume starting at the bottom of the stack."""
+        """Get the amount of liquid in the specified volume starting at the physical-bottom of the stack."""
         liquid_volume = 0.0
         for el in reversed(self._fluid_stack):
             if el.kind == FluidKind.LIQUID:
