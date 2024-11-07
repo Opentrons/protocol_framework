499e364294201e4edc76ca03390e95b9b6db627e
znaim <zair.naim@thoughtworks.com>
2024-11-07 13:22:33 -0500

    added a progress spinner when pressing submit

--- opentrons-ai-client/src/atoms/SendButton/index.tsx
+++ opentrons-ai-client/src/atoms/SendButton/index.tsx
@@ -7,14 +7,23 @@ import {
   COLORS,
   DISPLAY_FLEX,
   Icon,
-  JUSTIFY_CENTER,
+  JUSTIFY_SPACE_AROUND,
+  StyledText,
 } from '@opentrons/components'
+import { useEffect, useState } from 'react'
 
 interface SendButtonProps {
   handleClick: () => void
   disabled?: boolean
   isLoading?: boolean
 }
+
+const progressTexts = [
+  'Initializing...',
+  'Processing...',
+  'Generating...',
+  'Finalizing...',
+]
 
 export function SendButton({
   handleClick,
@@ -47,20 +56,47 @@ export function SendButton({
       color: ${COLORS.grey50};
     }
   `
+
+  const [buttonText, setButtonText] = useState(progressTexts[0])
+  const [, setProgressIndex] = useState(0)
+
+  useEffect(() => {
+    if (isLoading) {
+      const interval = setInterval(() => {
+        setProgressIndex(prevIndex => {
+          const newIndex = (prevIndex + 1) % progressTexts.length
+          setButtonText(progressTexts[newIndex])
+          return newIndex
+        })
+      }, 5000)
+
+      return () => {
+        setProgressIndex(0)
+        clearInterval(interval)
+      }
+    }
+  }, [isLoading])
+
   return (
     <Btn
       alignItems={ALIGN_CENTER}
       backgroundColor={disabled ? COLORS.grey35 : COLORS.blue50}
       borderRadius={BORDERS.borderRadiusFull}
       display={DISPLAY_FLEX}
-      justifyContent={JUSTIFY_CENTER}
-      width="4.25rem"
+      justifyContent={JUSTIFY_SPACE_AROUND}
+      paddingX="20px"
+      width={isLoading ? 'wrap' : '4.25rem'}
       height="3.75rem"
       disabled={disabled || isLoading}
       onClick={handleClick}
       aria-label="play"
       css={playButtonStyle}
     >
+      {isLoading ? (
+        <StyledText paddingLeft="0px" paddingRight="24px" as="i">
+          {buttonText}
+        </StyledText>
+      ) : null}
       <Icon
         color={disabled ? COLORS.grey50 : COLORS.white}
         name={isLoading ? 'ot-spinner' : 'send'}
