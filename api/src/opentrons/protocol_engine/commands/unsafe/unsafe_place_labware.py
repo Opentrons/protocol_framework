fe95bf02b23c0d740f776e70f42e922d46bb0e63
Brayan Almonte <vegano1@users.noreply.github.com>
2024-11-13 10:31:34 -0500

    Update api/src/opentrons/protocol_engine/commands/unsafe/unsafe_place_labware.py
    Co-authored-by: Max Marrone <max@opentrons.com>

--- api/src/opentrons/protocol_engine/commands/unsafe/unsafe_place_labware.py
+++ api/src/opentrons/protocol_engine/commands/unsafe/unsafe_place_labware.py
@@ -131,6 +131,9 @@ class UnsafePlaceLabwareImplementation(
 
         # NOTE: When the estop is pressed, the gantry loses position, lets use
         # the encoders to sync position.
+        # Ideally, we'd do a full home, but this command is used when
+        # the gripper is holding the plate reader, and a full home would
+        # bang it into the right window.
         await ot3api.home(axes=[Axis.Z_L, Axis.Z_R, Axis.Z_G])
         await ot3api.engage_axes([Axis.X, Axis.Y])
         await ot3api.update_axis_position_estimations([Axis.X, Axis.Y])
