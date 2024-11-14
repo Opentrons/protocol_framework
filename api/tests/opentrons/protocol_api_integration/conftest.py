4cb87f6db28ca7a01b05f2ce78415bc06d1a2a76
jbleon95 <jeremybenleon@gmail.com>
2024-11-14 14:23:47 -0500

    todo comment to eventually fix this plus covering non-PE contexts

--- api/tests/opentrons/protocol_api_integration/conftest.py
+++ api/tests/opentrons/protocol_api_integration/conftest.py
@@ -5,6 +5,7 @@ from _pytest.fixtures import SubRequest
 from typing import Generator
 
 from opentrons import simulate, protocol_api
+from opentrons.protocol_api.core.engine import ENGINE_CORE_API_VERSION
 
 
 @pytest.fixture
@@ -17,4 +18,11 @@ def simulated_protocol_context(
     try:
         yield context
     finally:
-        simulate._LIVE_PROTOCOL_ENGINE_CONTEXTS.close()
+        if context.api_version >= ENGINE_CORE_API_VERSION:
+            # TODO(jbl, 2024-11-14) this is a hack of a hack to close the hardware and the PE thread when a test is
+            #  complete. At some point this should be replaced with a more holistic way of safely cleaning up these
+            #  threads so they don't leak and cause tests to fail when `get_protocol_api` is called too many times.
+            simulate._LIVE_PROTOCOL_ENGINE_CONTEXTS.close()
+        else:
+            # If this is a non-PE context we need to clean up the hardware thread manually
+            context._hw_manager.hardware.clean_up()
