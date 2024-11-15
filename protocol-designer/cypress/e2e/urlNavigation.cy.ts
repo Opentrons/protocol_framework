2b8b9f9ed87a2b5ee78a2019405c6101491197c5
Josh McVey <yersh13@gmail.com>
2024-11-15 07:42:02 -0600

    remove todo

--- protocol-designer/cypress/e2e/urlNavigation.cy.ts
+++ protocol-designer/cypress/e2e/urlNavigation.cy.ts
@@ -6,13 +6,11 @@ describe('URL Navigation', () => {
   it('createNew', () => {
     cy.visit('#/createNew')
     // directly navigating sends you back to the home page
-    // TODO: Is this correct?
     cy.verifyHomePage()
   })
   it('overview', () => {
     cy.visit('#/overview')
     // directly navigating sends you back to the home page
-    // TODO: Is this correct?
     cy.verifyHomePage()
   })
 })
