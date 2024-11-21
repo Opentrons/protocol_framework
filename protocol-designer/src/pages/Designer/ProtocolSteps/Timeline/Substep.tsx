75416f1e046c3d69cc412461a76b153db0793991
Jethary <jethary@opentrons.com>
2024-11-21 17:01:55 -0500

    remove liquid icon, uppercase text

--- protocol-designer/src/pages/Designer/ProtocolSteps/Timeline/Substep.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/Timeline/Substep.tsx
@@ -1,38 +1,24 @@
 import * as React from 'react'
 import { useTranslation } from 'react-i18next'
-import { useSelector } from 'react-redux'
 import noop from 'lodash/noop'
-import { AIR } from '@opentrons/step-generation'
 import {
   ALIGN_CENTER,
-  COLORS,
   DIRECTION_COLUMN,
   DeckInfoLabel,
   Flex,
   JUSTIFY_SPACE_BETWEEN,
-  LiquidIcon,
   ListItem,
   SPACING,
   StyledText,
   Tag,
 } from '@opentrons/components'
-import { selectors } from '../../../../labware-ingred/selectors'
-import {
-  MIXED_WELL_COLOR,
-  swatchColors,
-} from '../../../../components/swatchColors'
 import { compactPreIngreds, formatVolume } from './utils'
 import type { AdditionalEquipmentName } from '@opentrons/step-generation'
 
-import type {
-  SubstepIdentifier,
-  SubstepWellData,
-  WellIngredientNames,
-} from '../../../../steplist'
+import type { SubstepIdentifier, SubstepWellData } from '../../../../steplist'
 
 interface SubstepProps {
   trashName: AdditionalEquipmentName | null
-  ingredNames: WellIngredientNames
   stepId: string
   substepIndex: number
   volume?: number | string | null
@@ -45,7 +31,6 @@ interface SubstepProps {
 function SubstepComponent(props: SubstepProps): JSX.Element {
   const {
     volume,
-    ingredNames,
     stepId,
     substepIndex,
     source,
@@ -54,7 +39,11 @@ function SubstepComponent(props: SubstepProps): JSX.Element {
     selectSubstep: propSelectSubstep,
     isSameLabware,
   } = props
-  const { t } = useTranslation(['application', 'protocol_steps', 'shared'])
+  const { i18n, t } = useTranslation([
+    'application',
+    'protocol_steps',
+    'shared',
+  ])
   const compactedSourcePreIngreds = source
     ? compactPreIngreds(source.preIngreds)
     : {}
@@ -62,15 +51,6 @@ function SubstepComponent(props: SubstepProps): JSX.Element {
   const selectSubstep = propSelectSubstep ?? noop
 
   const ingredIds: string[] = Object.keys(compactedSourcePreIngreds)
-  const liquidDisplayColors = useSelector(selectors.getLiquidDisplayColors)
-  const noColor = ingredIds.filter(id => id !== AIR).length === 0
-  let color = MIXED_WELL_COLOR
-  if (ingredIds.length === 1) {
-    color =
-      liquidDisplayColors[Number(ingredIds[0])] ?? swatchColors(ingredIds[0])
-  } else if (noColor) {
-    color = COLORS.transparent
-  }
 
   const volumeTag = (
     <Tag
@@ -104,16 +84,6 @@ function SubstepComponent(props: SubstepProps): JSX.Element {
             width="100%"
             alignItems={ALIGN_CENTER}
           >
-            {ingredIds.length > 0 ? (
-              <Flex gridGap={SPACING.spacing4} alignItems={ALIGN_CENTER}>
-                <LiquidIcon color={color} size="medium" />
-
-                <StyledText desktopStyle="bodyDefaultRegular">
-                  {ingredIds.map(groupId => ingredNames[groupId]).join(',')}
-                </StyledText>
-              </Flex>
-            ) : null}
-
             <Flex gridGap={SPACING.spacing4} alignItems={ALIGN_CENTER}>
               <StyledText desktopStyle="bodyDefaultRegular">
                 {t('protocol_steps:mix')}
@@ -123,9 +93,12 @@ function SubstepComponent(props: SubstepProps): JSX.Element {
                 {t('protocol_steps:in')}
               </StyledText>
               <DeckInfoLabel
-                deckLabel={t('protocol_steps:well_name', {
-                  wellName: source?.well ?? '',
-                })}
+                deckLabel={i18n.format(
+                  t('protocol_steps:well_name', {
+                    wellName: source?.well ?? '',
+                  }),
+                  'upperCase'
+                )}
               />
             </Flex>
           </Flex>
@@ -141,16 +114,6 @@ function SubstepComponent(props: SubstepProps): JSX.Element {
                 width="100%"
                 alignItems={ALIGN_CENTER}
               >
-                {ingredIds.length > 0 ? (
-                  <Flex gridGap={SPACING.spacing4} alignItems={ALIGN_CENTER}>
-                    <LiquidIcon color={color} size="medium" />
-
-                    <StyledText desktopStyle="bodyDefaultRegular">
-                      {ingredIds.map(groupId => ingredNames[groupId]).join(',')}
-                    </StyledText>
-                  </Flex>
-                ) : null}
-
                 <Flex gridGap={SPACING.spacing4} alignItems={ALIGN_CENTER}>
                   <StyledText desktopStyle="bodyDefaultRegular">
                     {t('protocol_steps:aspirated')}
@@ -160,9 +123,12 @@ function SubstepComponent(props: SubstepProps): JSX.Element {
                     {t('protocol_steps:from')}
                   </StyledText>
                   <DeckInfoLabel
-                    deckLabel={t('protocol_steps:well_name', {
-                      wellName: source.well,
-                    })}
+                    deckLabel={i18n.format(
+                      t('protocol_steps:well_name', {
+                        wellName: source.well,
+                      }),
+                      'upperCase'
+                    )}
                   />
                 </Flex>
               </Flex>
@@ -177,14 +143,6 @@ function SubstepComponent(props: SubstepProps): JSX.Element {
                 width="100%"
                 alignItems={ALIGN_CENTER}
               >
-                {ingredIds.length > 0 ? (
-                  <Flex gridGap={SPACING.spacing4} alignItems={ALIGN_CENTER}>
-                    <LiquidIcon color={color} size="medium" />
-                    <StyledText desktopStyle="bodyDefaultRegular">
-                      {ingredIds.map(groupId => ingredNames[groupId]).join(',')}
-                    </StyledText>
-                  </Flex>
-                ) : null}
                 {dest != null || trashName != null ? (
                   <Flex gridGap={SPACING.spacing4} alignItems={ALIGN_CENTER}>
                     <StyledText desktopStyle="bodyDefaultRegular">
@@ -196,13 +154,14 @@ function SubstepComponent(props: SubstepProps): JSX.Element {
                     </StyledText>
 
                     <DeckInfoLabel
-                      deckLabel={
+                      deckLabel={i18n.format(
                         dest?.well != null
                           ? t('protocol_steps:well_name', {
                               wellName: dest.well,
                             })
-                          : t(`shared:${trashName}`)
-                      }
+                          : t(`shared:${trashName}`),
+                        'upperCase'
+                      )}
                     />
                   </Flex>
                 ) : null}
