e4b42a923f54d9d05b4dec1983a0dfe991806985
koji <kk2796@nyu.edu>
2024-11-22 13:50:29 -0500

    fix test errors

--- protocol-designer/src/resources/__tests__/useScreenSizeCheck.test.ts
+++ protocol-designer/src/resources/__tests__/useScreenSizeCheck.test.ts
@@ -1,6 +1,6 @@
-// import { describe, it, vi, expect } from 'vitest'
-// import { renderHook } from '@testing-library/react'
-// import { useScreenSizeCheck } from '../useScreenSizeCheck'
+import { describe, it, vi, expect, beforeEach, afterEach } from 'vitest'
+import { renderHook, act } from '@testing-library/react'
+import { useScreenSizeCheck } from '../useScreenSizeCheck'
 
 describe('useScreenSizeCheck', () => {
   // it('should return true if the window size is greater than 600px x 650px', () => {
@@ -37,4 +37,72 @@ describe('useScreenSizeCheck', () => {
   //   const { result } = renderHook(() => useScreenSizeCheck())
   //   expect(result.current).toBe(false)
   // })
+  const originalWidth = window.screen.width
+  const originalHeight = window.screen.height
+
+  beforeEach(() => {
+    Object.defineProperty(window.screen, 'width', {
+      writable: true,
+      configurable: true,
+      value: originalWidth,
+    })
+    Object.defineProperty(window.screen, 'height', {
+      writable: true,
+      configurable: true,
+      value: originalHeight,
+    })
+  })
+
+  afterEach(() => {
+    Object.defineProperty(window.screen, 'width', {
+      writable: true,
+      configurable: true,
+      value: originalWidth,
+    })
+    Object.defineProperty(window.screen, 'height', {
+      writable: true,
+      configurable: true,
+      value: originalHeight,
+    })
+  })
+
+  it('should return true if screen size is larger than breakpoints', () => {
+    Object.defineProperty(window.screen, 'width', { value: 800 })
+    Object.defineProperty(window.screen, 'height', { value: 700 })
+
+    const { result } = renderHook(() => useScreenSizeCheck())
+
+    expect(result.current).toBe(true)
+  })
+
+  it('should return false if screen size is smaller than breakpoints', () => {
+    Object.defineProperty(window.screen, 'width', { value: 700 })
+    Object.defineProperty(window.screen, 'height', { value: 600 })
+
+    const { result } = renderHook(() => useScreenSizeCheck())
+
+    expect(result.current).toBe(false)
+  })
+
+  it('should update value on window resize', () => {
+    const { result } = renderHook(() => useScreenSizeCheck())
+
+    expect(result.current).toBe(originalWidth > 768 && originalHeight > 650)
+
+    act(() => {
+      Object.defineProperty(window.screen, 'width', { value: 800 })
+      Object.defineProperty(window.screen, 'height', { value: 700 })
+      window.dispatchEvent(new Event('resize'))
+    })
+
+    expect(result.current).toBe(true)
+
+    act(() => {
+      Object.defineProperty(window.screen, 'width', { value: 700 })
+      Object.defineProperty(window.screen, 'height', { value: 600 })
+      window.dispatchEvent(new Event('resize'))
+    })
+
+    expect(result.current).toBe(false)
+  })
 })
