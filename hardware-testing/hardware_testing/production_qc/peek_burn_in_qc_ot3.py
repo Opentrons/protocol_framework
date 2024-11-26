da66aa5fdfe870bf8bee796e32254b130f5abfba
Jerome <1458798121@qq.com>
2024-11-26 08:54:03 +0800

    cycle burn in test 4 times

--- hardware-testing/hardware_testing/production_qc/peek_burn_in_qc_ot3.py
+++ hardware-testing/hardware_testing/production_qc/peek_burn_in_qc_ot3.py
@@ -435,7 +435,7 @@ async def _main(is_simulating: bool, cycles: int, trials: int, continue_after_st
             dut = helpers_ot3.DeviceUnderTest.by_mount(mount)
             helpers_ot3.set_csv_report_meta_data_ot3(api, report, dut)
 
-            for cycle in range(0, cycles+TRIALS_PER_CYCLE, TRIALS_PER_CYCLE):
+            for cycle in range(TRIALS_PER_CYCLE, cycles+TRIALS_PER_CYCLE):
                 await _test_plunger(
                     api, mount, report,
                     cycle=cycle, trials=trials,
