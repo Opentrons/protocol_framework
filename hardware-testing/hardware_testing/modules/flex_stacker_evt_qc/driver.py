2b4e6575460591d93c0c399b98454cfee3b6bd95
Brayan Almonte <vegano1@users.noreply.github.com>
2024-11-27 16:22:57 -0500

    fix(hardware-testing): add __init__.py to modules/ so it gets packed in the build + other Flex stacker QC fixes. (#16995)
    - add an `__init__.py` so it does, this will make it so we can push to the robot.
    - change the way we validate no movement when in e-stop
    - don't wait for ack from stop_motor `M0` command.

--- hardware-testing/hardware_testing/modules/flex_stacker_evt_qc/driver.py
+++ hardware-testing/hardware_testing/modules/flex_stacker_evt_qc/driver.py
@@ -119,10 +119,15 @@ class FlexStacker:
         if not self._simulating:
             self._serial = serial.Serial(port, baudrate=STACKER_FREQ, timeout=60)
 
-    def _send_and_recv(self, msg: str, guard_ret: str = "") -> str:
+    def _send_and_recv(
+        self, msg: str, guard_ret: str = "", response_required: bool = True
+    ) -> str:
         """Internal utility to send a command and receive the response."""
         assert not self._simulating
+        self._serial.reset_input_buffer()
         self._serial.write(msg.encode())
+        if not response_required:
+            return "OK\n"
         ret = self._serial.readline()
         if guard_ret:
             if not ret.startswith(guard_ret.encode()):
@@ -159,7 +164,7 @@ class FlexStacker:
         """Stop motor movement."""
         if self._simulating:
             return
-        self._send_and_recv("M0\n")
+        self._send_and_recv("M0\n", response_required=False)
 
     def get_limit_switch(self, axis: StackerAxis, direction: Direction) -> bool:
         """Get limit switch status.
