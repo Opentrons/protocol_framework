a3b377bd959513ac52a6083b9a082453f169ffdc
Jethary <jethary@opentrons.com>
2024-12-02 12:03:34 -0500

    fix(protocol-designer): display correct z-value position in tip position field
    closes RQA-3723

--- protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/PositionField.tsx
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/PositionField.tsx
@@ -108,12 +108,17 @@ export function PositionField(props: PositionFieldProps): JSX.Element {
     zValue =
       mmFromBottom ?? getDefaultMmFromBottom({ name: zName, wellDepthMm })
   }
+
+  console.log(
+    mmFromBottom,
+    getDefaultMmFromBottom({ name: zName, wellDepthMm })
+  )
   let modal = (
     <ZTipPositionModal
       name={zName}
       closeModal={handleClose}
       wellDepthMm={wellDepthMm}
-      zValue={mmFromBottom}
+      zValue={zValue as number}
       updateValue={zUpdateValue}
       isIndeterminate={isIndeterminate}
     />
@@ -133,7 +138,7 @@ export function PositionField(props: PositionFieldProps): JSX.Element {
     const specs: PositionSpecs = {
       z: {
         name: zName,
-        value: mmFromBottom,
+        value: zValue as number,
         updateValue: zUpdateValue,
       },
       x: {
@@ -198,7 +203,7 @@ export function PositionField(props: PositionFieldProps): JSX.Element {
                   propsForFields[yField].value != null
                     ? Number(propsForFields[yField].value)
                     : 0,
-                z: mmFromBottom ?? 0,
+                z: zValue,
               })}
             </StyledText>
           </ListButton>
