64fbf4226e52cf3aa3a508cf8e54fe199003d740
Max Marrone <max@opentrons.com>
2024-12-03 16:55:21 -0500

    refactor(api): Port `ReadAbsorbanceImpl` and `FileStore` to `StateUpdate` (#16978)

--- api/src/opentrons/protocol_engine/commands/absorbance_reader/read.py
+++ api/src/opentrons/protocol_engine/commands/absorbance_reader/read.py
@@ -16,6 +16,7 @@ from ...resources.file_provider import (
     MAXIMUM_CSV_FILE_LIMIT,
 )
 from ...resources import FileProvider
+from ...state import update_types
 
 if TYPE_CHECKING:
     from opentrons.protocol_engine.state.state import StateView
@@ -143,7 +144,7 @@ class ReadAbsorbanceImpl(
         # Today, the action handler for the FileStore looks for a ReadAbsorbanceResult command action, this will need to be delinked.
 
         # Begin interfacing with the file provider if the user provided a filename
-        file_ids = []
+        file_ids: list[str] = []
         if params.fileName is not None:
             # Create the Plate Reader Transform
             plate_read_result = PlateReaderData.construct(
@@ -175,7 +176,13 @@ class ReadAbsorbanceImpl(
                 )
 
         return SuccessData(
-            public=ReadAbsorbanceResult(data=asbsorbance_result, fileIds=file_ids),
+            public=ReadAbsorbanceResult(
+                data=asbsorbance_result,
+                fileIds=file_ids,
+            ),
+            state_update=update_types.StateUpdate(
+                files_added=update_types.FilesAddedUpdate(file_ids=file_ids)
+            ),
         )
 
 
--- api/src/opentrons/protocol_engine/state/update_types.py
+++ api/src/opentrons/protocol_engine/state/update_types.py
@@ -98,7 +98,7 @@ class LabwareLocationUpdate:
     new_location: LabwareLocation
     """The labware's new location."""
 
-    offset_id: typing.Optional[str]
+    offset_id: str | None
     """The ID of the labware's new offset, for its new location."""
 
 
@@ -112,10 +112,10 @@ class LoadedLabwareUpdate:
     new_location: LabwareLocation
     """The labware's initial location."""
 
-    offset_id: typing.Optional[str]
+    offset_id: str | None
     """The ID of the labware's offset."""
 
-    display_name: typing.Optional[str]
+    display_name: str | None
 
     definition: LabwareDefinition
 
@@ -133,7 +133,7 @@ class LoadPipetteUpdate:
 
     pipette_name: PipetteNameType
     mount: MountType
-    liquid_presence_detection: typing.Optional[bool]
+    liquid_presence_detection: bool | None
 
 
 @dataclasses.dataclass
@@ -162,7 +162,7 @@ class PipetteTipStateUpdate:
     """Update pipette tip state."""
 
     pipette_id: str
-    tip_geometry: typing.Optional[TipGeometry]
+    tip_geometry: TipGeometry | None
 
 
 @dataclasses.dataclass
@@ -262,6 +262,13 @@ class LiquidClassLoadedUpdate:
 
 
 @dataclasses.dataclass
+class FilesAddedUpdate:
+    """An update that adds a new data file."""
+
+    file_ids: list[str]
+
+
+@dataclasses.dataclass
 class StateUpdate:
     """Represents an update to perform on engine state."""
 
@@ -298,6 +305,8 @@ class StateUpdate:
     absorbance_reader_lid: AbsorbanceReaderLidUpdate | NoChangeType = NO_CHANGE
 
     liquid_class_loaded: LiquidClassLoadedUpdate | NoChangeType = NO_CHANGE
+
+    files_added: FilesAddedUpdate | NoChangeType = NO_CHANGE
 
     def append(self, other: Self) -> Self:
         """Apply another `StateUpdate` "on top of" this one.
@@ -378,7 +387,6 @@ class StateUpdate:
                 new_deck_point=new_deck_point,
             )
         else:
-
             self.pipette_location = PipetteLocationUpdate(
                 pipette_id=pipette_id,
                 new_location=Well(labware_id=new_labware_id, well_name=new_well_name),
@@ -410,8 +418,8 @@ class StateUpdate:
         self: Self,
         definition: LabwareDefinition,
         labware_id: str,
-        offset_id: typing.Optional[str],
-        display_name: typing.Optional[str],
+        offset_id: str | None,
+        display_name: str | None,
         location: LabwareLocation,
     ) -> Self:
         """Add a new labware to state. See `LoadedLabwareUpdate`."""
@@ -429,7 +437,7 @@ class StateUpdate:
         pipette_id: str,
         pipette_name: PipetteNameType,
         mount: MountType,
-        liquid_presence_detection: typing.Optional[bool],
+        liquid_presence_detection: bool | None,
     ) -> Self:
         """Add a new pipette to state. See `LoadPipetteUpdate`."""
         self.loaded_pipette = LoadPipetteUpdate(
@@ -462,7 +470,7 @@ class StateUpdate:
         return self
 
     def update_pipette_tip_state(
-        self: Self, pipette_id: str, tip_geometry: typing.Optional[TipGeometry]
+        self: Self, pipette_id: str, tip_geometry: TipGeometry | None
     ) -> Self:
         """Update a pipette's tip state. See `PipetteTipStateUpdate`."""
         self.pipette_tip_state = PipetteTipStateUpdate(
