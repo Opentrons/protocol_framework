6f16f9126588cb0f33488f91de8db737c7bdaef7
Ryan Howard <rjhoward1692@gmail.com>
2024-12-05 14:24:36 -0500

    fix the analysis

--- hardware-testing/hardware_testing/protocols/gravimetric_lpc/gravimetric/gravimetric_ot3_p200_96.py
+++ hardware-testing/hardware_testing/protocols/gravimetric_lpc/gravimetric/gravimetric_ot3_p200_96.py
@@ -3,7 +3,7 @@ from opentrons.protocol_api import ProtocolContext
 from opentrons.protocol_api._types import OffDeckType
 
 metadata = {"protocolName": "gravimetric-ot3-p200-96"}
-requirements = {"robotType": "Flex", "apiLevel": "2.21"}
+requirements = {"robotType": "Flex", "apiLevel": "2.15"}
 
 SLOT_SCALE = 4
 SLOTS_TIPRACK = {
