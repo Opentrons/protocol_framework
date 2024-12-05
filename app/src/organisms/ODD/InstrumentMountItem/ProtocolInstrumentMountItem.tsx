84020c2adcad13d494d37979e430b62a12122146
Ryan Howard <rjhoward1692@gmail.com>
2024-12-05 16:42:37 -0500

    try and find all the hardcoded p1000_96

--- app/src/organisms/ODD/InstrumentMountItem/ProtocolInstrumentMountItem.tsx
+++ app/src/organisms/ODD/InstrumentMountItem/ProtocolInstrumentMountItem.tsx
@@ -76,8 +76,9 @@ export function ProtocolInstrumentMountItem(
     []
   )
   const [flowType, setFlowType] = useState<string>(FLOWS.ATTACH)
+  const is96ChannelPipette = speccedName === 'p1000_96' || speccedName === "p200_96"
   const selectedPipette =
-    speccedName === 'p1000_96' ? NINETY_SIX_CHANNEL : SINGLE_MOUNT_PIPETTES
+    is96ChannelPipette ? NINETY_SIX_CHANNEL : SINGLE_MOUNT_PIPETTES
 
   const handleCalibrate: MouseEventHandler = () => {
     setFlowType(FLOWS.CALIBRATE)
@@ -95,7 +96,7 @@ export function ProtocolInstrumentMountItem(
       setShowPipetteWizardFlow(true)
     }
   }
-  const is96ChannelPipette = speccedName === 'p1000_96'
+
   const isAttachedWithCal =
     attachedInstrument != null &&
     attachedInstrument.ok &&
