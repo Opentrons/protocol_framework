f8263cbbcc895a91f64eb2d5675ccb3f9b7c98e8
Max Marrone <max@opentrons.com>
2024-12-12 15:32:55 -0500

    Shore up HexColor tests for my paranoia.

--- api/tests/opentrons/protocol_engine/test_types.py
+++ api/tests/opentrons/protocol_engine/test_types.py
@@ -10,9 +10,13 @@ def test_hex_validation(hex_color: str) -> None:
     """Should allow creating a HexColor."""
     # make sure noting is raised when instantiating this class
     assert HexColor(hex_color)
+    assert HexColor.model_validate_json(f'"{hex_color}"')
 
 
-def test_handles_invalid_hex() -> None:
+@pytest.mark.parametrize("invalid_hex_color", ["true", "null", "#123456789"])
+def test_handles_invalid_hex(invalid_hex_color: str) -> None:
     """Should raise a validation error."""
     with pytest.raises(ValidationError):
-        HexColor("#123456789")
+        HexColor(invalid_hex_color)
+    with pytest.raises(ValidationError):
+        HexColor.model_validate_json(f'"{invalid_hex_color}"')
