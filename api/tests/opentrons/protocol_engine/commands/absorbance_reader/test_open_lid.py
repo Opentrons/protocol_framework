9c6e049480f5374076d5f568919320180d456c31
tamarzanzouri <tamar.zanzouri@opentrons.com>
2024-12-24 12:29:19 -0500

    unused ignore after setup

--- api/tests/opentrons/protocol_engine/commands/absorbance_reader/test_open_lid.py
+++ api/tests/opentrons/protocol_engine/commands/absorbance_reader/test_open_lid.py
@@ -38,10 +38,10 @@ from opentrons_shared_data.labware.labware_definition import (
 @pytest.fixture
 def absorbance_def() -> LabwareDefinition:
     """Get a tip rack Pydantic model definition value object."""
-    return LabwareDefinition.construct(  # type:ignore[call-arg]
+    return LabwareDefinition.construct(
         namespace="test",
         version=1,
-        parameters=Parameters.construct(  # type:ignore[call-arg]
+        parameters=Parameters.construct(
             loadName="cool-labware",
             tipOverlap=None,  # add a None value to validate serialization to dictionary
         ),
