bf9d1dcbcdc4dd2f85c5e5721d92641b49a1edf8
Jethary <jethary@opentrons.com>
2025-01-08 09:04:58 -0500

    remove console

--- protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/utils.ts
+++ protocol-designer/src/pages/Designer/ProtocolSteps/StepForm/PipetteFields/utils.ts
@@ -135,14 +135,21 @@ export function getDisabledPathMap(
     airGapVolume,
   })
 
-  if (!withinCapacityForMultiDispense && (values.volume != null && values.volume !== '')) {
+  if (
+    !withinCapacityForMultiDispense &&
+    values.volume != null &&
+    values.volume !== ''
+  ) {
     disabledPathMap = {
       ...disabledPathMap,
       multiDispense: t('step_edit_form.field.path.subtitle.volume_too_high'),
     }
   }
-console.log(values.volume == null, values.volume === '')
-  if (!withinCapacityForMultiAspirate && (values.volume != null && values.volume !== '')) {
+  if (
+    !withinCapacityForMultiAspirate &&
+    values.volume != null &&
+    values.volume !== ''
+  ) {
     disabledPathMap = {
       ...disabledPathMap,
       multiAspirate: t('step_edit_form.field.path.subtitle.volume_too_high'),
