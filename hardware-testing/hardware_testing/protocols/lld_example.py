ef588460a24ab039de98f0bdbf3011ae95a31476
Andy Sigler <andrewsigler1@gmail.com>
2025-01-09 14:20:59 -0500

    wip

--- hardware-testing/hardware_testing/protocols/lld_example.py
+++ hardware-testing/hardware_testing/protocols/lld_example.py
@@ -1,10 +1,20 @@
 """LLD Example."""
-from opentrons.protocol_api import ProtocolContext, ParameterContext
+from typing import List
+
+from opentrons.protocol_api import (
+    ProtocolContext,
+    ParameterContext,
+    Parameters,
+    Labware,
+    Well,
+)
 
 from hardware_testing.opentrons_api import runtime_parameters as rtp
 
 
-DILUENT_DEAD_VOL = 3000
+# these will probably never change, determined by plate-reader
+MINIMUM_PLATE_READER_UL = 200
+DESTINATION_PLATE_NAME = "corning_96_wellplate_360ul_flat"
 
 
 def add_parameters(params: ParameterContext) -> None:
@@ -30,75 +40,83 @@ def add_parameters(params: ParameterContext) -> None:
     )
     rtp.add_parameter_slot(
         params,
-        label="optical_slot",
+        label="destination_slot",
         default_slot="D3",
     )
     params.add_int("starting_volume", "starting_volume", default=100, unit="uL")
     params.add_int("trials", "trials", default=48, unit="qty")
     params.add_float("aspirate_volume", "aspirate_volume", default=1.0, unit="uL")
     params.add_float("submerge", "submerge", default=-1.5, unit="mm")
+    params.add_int("diluent_dead_vol", "diluent_dead_vol", default=3, unit="mL")
+    params.add_int(
+        "destination_start_row",
+        "destination_start_row",
+        default=1,
+        minimum=1,
+        maximum=12,
+    )
     params.add_str("dye_well", "dye_well", default="A1")
     params.add_str("diluent_well", "diluent_well", default="A1")
 
 
-def run(ctx: ProtocolContext) -> None:
-    p = ctx.params
-
-    # load labware
-    labware_red_dye = ctx.load_labware(
-        load_name=p.red_dye_src_name, location=p.red_dye_src_slot
-    )
-    labware_red_dye.load_empty()
-    labware_diluent = ctx.load_labware(
-        load_name=p.diluent_src_name, location=p.diluent_src_slot
-    )
-    labware_diluent.load_empty()
-    labware_optical = ctx.load_labware(
-        load_name="corning_96_wellplate_360ul_flat", location=p.optical_slot
-    )
-    labware_optical.load_empty()
-
-    # load liquid
-    red_dye = ctx.define_liquid("red-dye", display_color="#FF0000")
-    labware_red_dye.load_liquid([p.src_well], p.starting_volume, red_dye)
+def spread_diluent(
+    ctx: ProtocolContext, p: Parameters, destination_wells: List[Well]
+) -> None:
+    diluent_ul_per_well = MINIMUM_PLATE_READER_UL - p.aspirate_volume
+    assert diluent_ul_per_well > 0
     blue_diluent = ctx.define_liquid("blue-diluent", display_color="#0000FF")
-    diluent_ul_per_well = max(200 - p.aspirate_volume, 0)
-    if diluent_ul_per_well:
-        diluent_ul_required = diluent_ul_per_well * p.trials
-        diluent_ul_per_reservoir_src = (diluent_ul_required / 2) + DILUENT_DEAD_VOL
-        labware_diluent.load_liquid(
-            [p.diluent_well], diluent_ul_per_reservoir_src, blue_diluent
-        )
-
-    # load pipettes
-    pipette = ctx.load_instrument(
-        instrument_name=p.pipette_name,
-        mount=p.pipette_mount,
-        tip_racks=[ctx.load_labware(p.tiprack_name, p.tiprack_slot)],
-    )
     multi = ctx.load_instrument(
         instrument_name="flex_8channel_1000",
         mount={"left": "right", "right": "left"}[p.pip_mount],
         tip_racks=[ctx.load_labware(p.diluent_tiprack_name, p.diluent_tiprack_slot)],
     )
-
-    # spread the diluent
+    source_plate = ctx.load_labware(
+        load_name=p.diluent_src_name, location=p.diluent_src_slot
+    )
+    diluent_ul_required = (diluent_ul_per_well * p.trials) + p.diluent_dead_vol
+    source_plate.load_liquid([p.diluent_well], diluent_ul_required, blue_diluent)
     multi.pick_up_tip()
-    multi.require_liquid_presence(labware_diluent[p.diluent_well])
+    multi.require_liquid_presence(source_plate[p.diluent_well])
     for trial in range(0, p.trials, multi.channels):
         multi.aspirate(
-            diluent_ul_per_well, labware_diluent[p.diluent_well].meniscus(p.submerge)
+            diluent_ul_per_well, source_plate[p.diluent_well].meniscus(p.submerge)
         )
         multi.dispense(
-            multi.current_volume, labware_optical.wells()[trial].meniscus(p.submerge)
+            multi.current_volume, destination_wells[trial].meniscus(p.submerge)
         )
     multi.drop_tip()
 
-    # spread the dye
+
+def spread_red_dye(
+    ctx: ProtocolContext, p: Parameters, destination_wells: List[Well]
+) -> None:
+    red_dye = ctx.define_liquid("red-dye", display_color="#FF0000")
+    source_plate = ctx.load_labware(
+        load_name=p.red_dye_src_name, location=p.red_dye_src_slot
+    )
+    source_plate.load_liquid([p.src_well], p.starting_volume, red_dye)
+
+    pipette = ctx.load_instrument(
+        instrument_name=p.pipette_name,
+        mount=p.pipette_mount,
+        tip_racks=[ctx.load_labware(p.tiprack_name, p.tiprack_slot)],
+    )
     for trial in range(p.trials):
         pipette.pick_up_tip()
-        pipette.require_liquid_presence(labware_red_dye[p.dye_well])
-        asp_loc = labware_red_dye[p.dye_well].meniscus(p.aspirate_submerge)
+        pipette.require_liquid_presence(source_plate[p.dye_well])
+        asp_loc = source_plate[p.dye_well].meniscus(p.aspirate_submerge)
         pipette.aspirate(p.aspirate_volume, asp_loc)
-        disp_loc = labware_optical.wells()[trial].meniscus(p.submerge)
+        disp_loc = destination_wells[trial].meniscus(p.submerge)
         pipette.dispense(pipette.current_volume, disp_loc)
+
+
+def run(ctx: ProtocolContext) -> None:
+    destination_plate = ctx.load_labware(
+        load_name=DESTINATION_PLATE_NAME, location=ctx.params.destination_slot
+    )
+    destination_plate.load_empty()
+    start_well_idx = (ctx.params.destination_start_row - 1) * 8
+    destination_wells = destination_plate.wells()[start_well_idx:]
+    if ctx.params.aspirate_volume < MINIMUM_PLATE_READER_UL:
+        spread_diluent(ctx, ctx.params, destination_wells)
+    spread_red_dye(ctx, ctx.params, destination_wells)
