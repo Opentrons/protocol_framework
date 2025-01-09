5a9743d1d19457dae3d4efb6b79e03a0a9b706de
Max Marrone <max@opentrons.com>
2025-01-09 14:49:27 -0500

    Fill in the migration.
    ezpz.

--- robot-server/robot_server/persistence/_migrations/v8_to_v9.py
+++ robot-server/robot_server/persistence/_migrations/v8_to_v9.py
@@ -11,12 +11,14 @@ from robot_server.persistence.database import sql_engine_ctx
 from robot_server.persistence.file_and_directory_names import DB_FILE
 from robot_server.persistence.tables import schema_9
 
+from ._util import copy_contents
 from .._folder_migrator import Migration
 
 
 class Migration8to9(Migration):  # noqa: D101
     def migrate(self, source_dir: Path, dest_dir: Path) -> None:
         """Migrate the persistence directory from schema 8 to 9."""
-        # FIX BEFORE MERGE: Put an actual migration here.
+        copy_contents(source_dir=source_dir, dest_dir=dest_dir)
+
         with sql_engine_ctx(dest_dir / DB_FILE) as engine:
-            schema_9.metadata.create_all(engine)
+            schema_9.labware_offset_table.create(engine)
