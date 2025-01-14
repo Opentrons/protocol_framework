340e36687a3d39317ddc93f51b9d31e62dfac410
Seth Foster <seth@opentrons.com>
2025-01-14 10:20:48 -0500

    update: make defs a little more complex

--- shared-data/js/deck/index.ts
+++ shared-data/js/deck/index.ts
@@ -1,5 +1,5 @@
 import flexDeckDefV6 from '../../deck/definitions/6/ot3_standard.json'
 import ot2DeckDefV6 from '../../deck/definitions/6/ot2_standard.json'
-import ot2DeckDefShortFixedTrashV5 from '../../deck/definitions/6/ot2_short_trash.json'
+import ot2DeckDefShortFixedTrashV6 from '../../deck/definitions/6/ot2_short_trash.json'
 
 export { ot2DeckDefV6, ot2DeckDefShortFixedTrashV6, flexDeckDefV6 }
