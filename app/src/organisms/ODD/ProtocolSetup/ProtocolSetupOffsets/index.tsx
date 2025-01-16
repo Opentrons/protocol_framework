391edd8cef263148d29fa327eaf6d65117422a86
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-01-16 12:37:28 -0500

    nullish coalescence is a thing

--- app/src/organisms/ODD/ProtocolSetup/ProtocolSetupOffsets/index.tsx
+++ app/src/organisms/ODD/ProtocolSetup/ProtocolSetupOffsets/index.tsx
@@ -75,8 +75,7 @@ export function ProtocolSetupOffsets({
   const nonIdentityOffsets = getLatestCurrentOffsets(sortedOffsets)
   return (
     <>
-      {LPCWizard}
-      {LPCWizard == null && (
+      {LPCWizard ?? (
         <>
           <Flex
             flexDirection={DIRECTION_ROW}
