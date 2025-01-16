59bcddcd92968c4da7f51eb4783a7cdc6c1e281e
Jethary Alcid <66035149+jerader@users.noreply.github.com>
2025-01-16 15:29:55 -0500

    feat(components, app): move CommandText and TimelineScrubber components (#17284)
    This PR is part of very preliminary work to explore adding the timeline
    scrubber to other projects other than only the app. In order to start
    that work, the timeline scrubber & command text needed to be accessible
    outside of the app so I moved those components and affected components
    to the components directory.
    
    Additionally, I added `step-generation` as a module for `components` and
    I added `i18next` as a dependency, since command text has translations.

--- app/src/transformations/commands/transformations/index.ts
+++ app/src/transformations/commands/transformations/index.ts
@@ -1,6 +1,5 @@
 export * from './getLabwareSetupItemGroups'
 export * from './getProtocolUsesGripper'
-export * from './formatDuration'
 export * from './formatInterval'
 export * from './getModuleInitialLoadInfo'
 export * from './getNestedLabwareInfo'
