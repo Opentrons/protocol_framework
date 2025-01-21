fe62be348e0285202da8f1f3090fae39d10b8148
Andy Sigler <andrewsigler1@gmail.com>
2025-01-21 14:34:09 -0500

    simulates

--- hardware-testing/hardware_testing/protocols/liquid_sense/lld_1ul_pcr_well_to_mvs_plate.py
+++ hardware-testing/hardware_testing/protocols/liquid_sense/lld_1ul_pcr_well_to_mvs_plate.py
@@ -1,19 +1,32 @@
 from opentrons.protocol_api import ProtocolContext
 
+# MATERIALS:
+#  - 8ch manual pipette
+#  - P50S
+#  - 50uL tip-rack
+#  - Red dye (ideally Range D)
+#  - Diluent (or water)
+#  - PCR plate
+#  - MVS plate
+
+metadata = {"protocolName": "LLD 1uL PCR-to-MVS"}
+requirements = {"robotType": "Flex", "apiLevel": "2.22"}
+
 SLOTS = {
-    "tips": "D2",
-    "pcr": "C2",
-    "mvs": "C1"
+    "tips": "C3",
+    "mvs": "D3",
+    "pcr": "D2",
 }
 
 SUBMERGE_MM = -1.5
 TARGET_UL = 1
 TRIALS = 48
-SRC_WELL = "A1"
+SRC_WELL = "H1"
 DILUENT_UL = max(0, 200 - TARGET_UL)
 
 
 def run(ctx: ProtocolContext) -> None:
+    ctx.load_trash_bin("A3")
     tip_rack = ctx.load_labware("opentrons_flex_96_tiprack_50ul",
                                 location=SLOTS["tips"],
                                 version=1)
@@ -33,8 +46,8 @@ def run(ctx: ProtocolContext) -> None:
     pcr_plate.load_empty([w for w in pcr_plate.wells() if w.well_name != SRC_WELL])
     pcr_plate.load_liquid([pcr_plate[SRC_WELL]], TARGET_UL * TRIALS, dye)
 
-    pipette.pick_up_tip()
     for trial in range(TRIALS):
+        pipette.pick_up_tip()
         pipette.aspirate(TARGET_UL, pcr_plate[SRC_WELL].meniscus(SUBMERGE_MM))
         pipette.dispense(TARGET_UL, mvs_plate.wells()[trial].meniscus())
-    pipette.drop_tip()
+        pipette.drop_tip()
