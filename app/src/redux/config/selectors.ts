465b259b8d5e9a675b8bc55a21c4c2927f060dbc
smb2268 <sarah.breen@opentrons.com>
2025-01-24 11:42:48 -0500

    Persist null value when explicitly set by closing welcome flow

--- app/src/redux/config/selectors.ts
+++ app/src/redux/config/selectors.ts
@@ -139,7 +139,8 @@ export const getOnDeviceDisplaySettings: (
       unfinishedUnboxingFlowRoute:
         // @ts-expect-error special casing 0 because there is no null type that gnu make can provide at build time
         // see dev-shell-odd in app/Makefile (we provide 0 instead of null)
-        config.onDeviceDisplaySettings.unfinishedUnboxingFlowRoute !== 0
+        config.onDeviceDisplaySettings.unfinishedUnboxingFlowRoute !== 0 &&
+        config.onDeviceDisplaySettings.unfinishedUnboxingFlowRoute !== null
           ? '/choose-language'
           : null,
     }
