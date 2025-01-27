0dc1141a0f306f733dbfb8ddbdf1f6d07345a6bb
Andy Sigler <andrewsigler1@gmail.com>
2025-01-27 14:34:49 -0500

    cleaned up protocol a bit

--- hardware-testing/hardware_testing/protocols/liquid_sense/lld_test_1ul_pcr_well_every_time.py
+++ hardware-testing/hardware_testing/protocols/liquid_sense/lld_test_1ul_pcr_well_every_time.py
@@ -1,39 +1,35 @@
-from opentrons.protocol_api import ProtocolContext, InstrumentContext
-from opentrons_shared_data.pipette.load_data import load_definition
-from opentrons_shared_data.pipette.types import (
-    PipetteChannelType,
-    PipetteModelType,
-    PipetteVersionType,
-    PipetteOEMType,
-)
+from os import listdir
 
-# MATERIALS:
-#  - 8ch manual pipette
-#  - P50S
-#  - 50uL tip-rack
-#  - Red dye (ideally Range D)
-#  - Diluent (or water)
-#  - PCR plate
-#  - MVS plate
+from opentrons.protocol_api import ProtocolContext
+from opentrons_shared_data.load import get_shared_data_root
+
 
 metadata = {"protocolName": "LLD 1uL PCR-to-MVS"}
 requirements = {"robotType": "Flex", "apiLevel": "2.22"}
 
 SLOTS = {
     "tips": "C3",
-    "mvs": "D3",
-    "pcr": "D2",
+    "src": "D2",
+    "dst": "D3",
 }
 
-SUBMERGE_MM = -1.5
 TARGET_UL = 1
+SUBMERGE_MM = -1.5
 TRIALS = 48
-SRC_WELL = "H1"
 DILUENT_UL = max(0, 200 - TARGET_UL)
+
 TIP_VOLUME = 50
 PIP_VOLUME = 50
-LOWEST_POSSIBLE_TIP_MM = 0.5
 
+SRC_WELL = "H1"
+SRC_LABWARE = "opentrons_96_wellplate_200ul_pcr_full_skirt"
+DST_LABWARE = "corning_96_wellplate_360ul_flat"
+
+
+def get_latest_version(load_name: str) -> int:
+    labware_def_location = f"{get_shared_data_root()}/labware/definitions/3/{load_name}/"
+    labware_def_latest = sorted(listdir(labware_def_location))[-1]
+    return int(labware_def_latest[0])
 
 
 def run(ctx: ProtocolContext) -> None:
@@ -43,23 +39,29 @@ def run(ctx: ProtocolContext) -> None:
                                 version=1)
     pipette = ctx.load_instrument(f"flex_1channel_{PIP_VOLUME}",
                                   mount="left",
+                                  liquid_presence_detection=True,
                                   tip_racks=[tip_rack])
-    pcr_plate = ctx.load_labware("opentrons_96_wellplate_200ul_pcr_full_skirt",
-                                 location=SLOTS["pcr"],
-                                 version=3)
-    mvs_plate = ctx.load_labware("corning_96_wellplate_360ul_flat",
-                                 location=SLOTS["mvs"],
-                                 version=3)
+
+    # SRC and DST labware
+    pcr_plate = ctx.load_labware(SRC_LABWARE,
+                                 location=SLOTS["src"],
+                                 version=get_latest_version(SRC_LABWARE))
+    mvs_plate = ctx.load_labware(DST_LABWARE,
+                                 location=SLOTS["dst"],
+                                 version=get_latest_version(DST_LABWARE))
+
+    # define starting liquid volumes
     diluent = ctx.define_liquid("diluent", "#0000FF")
     dye = ctx.define_liquid("dye", "#FF0000")
     mvs_plate.load_liquid(mvs_plate.wells(), DILUENT_UL, diluent)
     pcr_plate.load_empty([w for w in pcr_plate.wells() if w.well_name != SRC_WELL])
     pcr_plate.load_liquid([pcr_plate[SRC_WELL]], TARGET_UL * TRIALS, dye)
 
+    # change tip before every aspirate
+    # NOTE: if liquid height is <2.5mm, protocol may error out
+    #       this can be avoided by adding extra starting liquid in the SRC labware
     for trial in range(TRIALS):
         pipette.pick_up_tip()
-        pipette.require_liquid_presence(pcr_plate[SRC_WELL])
-        src = pcr_plate[SRC_WELL].meniscus(SUBMERGE_MM)
-        pipette.aspirate(TARGET_UL, src)
-        pipette.dispense(TARGET_UL, mvs_plate.wells()[trial].meniscus())
+        pipette.aspirate(TARGET_UL, pcr_plate[SRC_WELL].meniscus(SUBMERGE_MM))
+        pipette.dispense(TARGET_UL, mvs_plate.wells()[trial].meniscus(SUBMERGE_MM))  # contact
         pipette.drop_tip()
