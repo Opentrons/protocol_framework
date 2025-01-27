728093f8926ae2ffbbcb9ec34ae1d443670719d8
rclarke0 <rhyann.clarke@opentrons.com>
2025-01-27 13:25:08 -0500

    remove meniscus relative

--- hardware-testing/hardware_testing/protocols/liquid_sense/lld_test_1ul_pcr_well_every_time.py
+++ hardware-testing/hardware_testing/protocols/liquid_sense/lld_test_1ul_pcr_well_every_time.py
@@ -35,19 +35,6 @@ PIP_VOLUME = 50
 LOWEST_POSSIBLE_TIP_MM = 0.5
 
 
-def _get_lld_min_height(pipette: InstrumentContext) -> float:
-    pip_model_list = pipette.model.split("_")
-    pip_def = load_definition(
-        model=PipetteModelType(pip_model_list[0]),
-        channels=PipetteChannelType(pipette.channels),
-        version=PipetteVersionType(
-            major=int(pip_model_list[-1][-3]),  # type: ignore[arg-type]
-            minor=int(pip_model_list[-1][-1]),  # type: ignore[arg-type]
-        ),
-        oem=PipetteOEMType.EM if "em" in pipette.model else PipetteOEMType.OT,
-    )
-    return pip_def.lld_settings[f"t{TIP_VOLUME}"]["minHeight"]
-
 
 def run(ctx: ProtocolContext) -> None:
     ctx.load_trash_bin("A3")
@@ -69,21 +56,10 @@ def run(ctx: ProtocolContext) -> None:
     pcr_plate.load_empty([w for w in pcr_plate.wells() if w.well_name != SRC_WELL])
     pcr_plate.load_liquid([pcr_plate[SRC_WELL]], TARGET_UL * TRIALS, dye)
 
-    min_aspirate_mm = _get_lld_min_height(pipette) - SUBMERGE_MM
-    meniscus_relative = True
     for trial in range(TRIALS):
         pipette.pick_up_tip()
-        if meniscus_relative:
-            pipette.require_liquid_presence(pcr_plate[SRC_WELL])
-        liquid_mm = pcr_plate[SRC_WELL]._core.  # TODO: get height of Liquid
-        if liquid_mm <= min_aspirate_mm:
-            meniscus_relative = False
-            src = pcr_plate[SRC_WELL].bottom(LOWEST_POSSIBLE_TIP_MM)
-        elif liquid_mm > LOWEST_POSSIBLE_TIP_MM:
-            src = pcr_plate[SRC_WELL].meniscus(SUBMERGE_MM)
-        else:
-            pipette.drop_tip()
-            break
+        pipette.require_liquid_presence(pcr_plate[SRC_WELL])
+        src = pcr_plate[SRC_WELL].meniscus(SUBMERGE_MM)
         pipette.aspirate(TARGET_UL, src)
         pipette.dispense(TARGET_UL, mvs_plate.wells()[trial].meniscus())
         pipette.drop_tip()
