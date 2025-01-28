57b01280129d2f9d3488d15e7e23c9c33bb1548a
tamarzanzouri <tamar.zanzouri@opentrons.com>
2025-01-28 14:37:18 -0500

    move append out of the loop

--- api/src/opentrons/protocol_engine/commands/absorbance_reader/read.py
+++ api/src/opentrons/protocol_engine/commands/absorbance_reader/read.py
@@ -181,10 +181,10 @@ class ReadAbsorbanceImpl(
                         )
                     )
                     file_ids.append(file_id)
-                    state_update.files_added = update_types.FilesAddedUpdate(
-                        file_ids=file_ids
-                    )
 
+                state_update.files_added = update_types.FilesAddedUpdate(
+                    file_ids=file_ids
+                )
                 # Return success data to api
                 return SuccessData(
                     public=ReadAbsorbanceResult(
