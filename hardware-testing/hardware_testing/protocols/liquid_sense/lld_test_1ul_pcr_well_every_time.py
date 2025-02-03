1540fd2714a5831642ccbc59d5967c581624d5c2
rclarke0 <rhyann.clarke@opentrons.com>
2025-02-03 11:14:11 -0500

    add baseline parameter

--- hardware-testing/hardware_testing/protocols/liquid_sense/lld_test_1ul_pcr_well_every_time.py
+++ hardware-testing/hardware_testing/protocols/liquid_sense/lld_test_1ul_pcr_well_every_time.py
@@ -1,10 +1,10 @@
 from os import listdir
 
-from opentrons.protocol_api import ProtocolContext, Well
+from opentrons.protocol_api import ProtocolContext, Well, ParameterContext
 from opentrons_shared_data.load import get_shared_data_root
 
 
-metadata = {"protocolName": "LLD 1uL PCR-to-MVS-SUBMERGE-8"}
+metadata = {"protocolName": "LLD 1uL PCR-to-MVS-WITH-BASELINE-PARAM"}
 requirements = {"robotType": "Flex", "apiLevel": "2.22"}
 
 SLOTS = {
@@ -16,17 +16,33 @@ SLOTS = {
 TARGET_UL = 1
 SUBMERGE_MM = -1.5
 BOTTOM_MM = 3.0
-TRIALS = int(96 / 4)  # define number of trials based on how to slice up the DST plate
 DILUENT_UL = max(0, 200 - TARGET_UL)
 
 TIP_VOLUME = 50
 PIP_VOLUME = 50
+DEAD_VOL_DILUENT = 3000
+DEAD_VOL_DYE = 5
+TRIALS = 8
 
 SRC_WELL = "H1"
 SRC_LABWARE = "opentrons_96_wellplate_200ul_pcr_full_skirt"
 DST_LABWARE = "corning_96_wellplate_360ul_flat"
 DILUENT_LABWARE = "nest_12_reservoir_15ml"
 
+def add_parameters(parameters: ParameterContext) -> None:
+    """Add parameters."""
+    parameters.add_int(
+        variable_name="columns",
+        display_name="Number of Columns",
+        minimum=1,
+        maximum=12,
+        default=12,
+    )
+    parameters.add_bool(
+        variable_name = "baseline",
+        display_name = "Baseline", 
+        default = False
+    )
 
 def get_latest_version(load_name: str) -> int:
     labware_def_location = (
@@ -37,7 +53,11 @@ def get_latest_version(load_name: str) -> int:
 
 
 def run(ctx: ProtocolContext) -> None:
+    global TRIALS, DILUENT_UL
     ctx.load_trash_bin("A3")
+    columns = ctx.params.columns
+    baseline = ctx.params.baseline
+    TRIALS = columns * 8
     tip_rack = ctx.load_labware(
         f"opentrons_flex_96_tiprack_{TIP_VOLUME}ul", location=SLOTS["tips"], version=1
     )
@@ -59,11 +79,14 @@ def run(ctx: ProtocolContext) -> None:
     # load diluent wells
     diluent = ctx.define_liquid("diluent", "#0000FF")
     diluent_wells = [diluent_reservoir["A1"], diluent_reservoir["A2"]]
-    diluent_reservoir.load_liquid(diluent_wells, 12000, diluent)
+    total_vol_per_well = (DILUENT_UL * TRIALS ) / len(diluent_wells)
+
+    diluent_reservoir.load_liquid(diluent_wells, total_vol_per_well + DEAD_VOL_DILUENT, diluent)
+    
     # load dye
     dye = ctx.define_liquid("dye", "#FF0000")
     src_labware.load_empty([w for w in src_labware.wells() if w.well_name != SRC_WELL])
-    src_labware.load_liquid([src_labware[SRC_WELL]], TARGET_UL * TRIALS, dye)
+    src_labware.load_liquid([src_labware[SRC_WELL]], (TARGET_UL * TRIALS) + DEAD_VOL_DYE, dye)
     dst_labware.load_empty(dst_labware.wells())
     
 
@@ -85,8 +108,8 @@ def run(ctx: ProtocolContext) -> None:
         
     # fill with diluent
     ctx.comment("FILLING DESTINATION PLATE WITH DILUENT")
-
-
+    if baseline:
+        DILUENT_UL +=TARGET_UL
     for i in range(int(TRIALS/8)):
         diluent_well = diluent_wells[ i % len(diluent_wells)]
         if i < len(diluent_wells):
@@ -94,9 +117,10 @@ def run(ctx: ProtocolContext) -> None:
                 diluent_pipette.return_tip()
             diluent_pipette.pick_up_tip()
             diluent_pipette.require_liquid_presence(diluent_well)
-        diluent_pipette.aspirate(DILUENT_UL, diluent_well.meniscus(-8))
+        diluent_pipette.aspirate(DILUENT_UL, diluent_well.bottom(.5))
         diluent_pipette.dispense(DILUENT_UL, dst_labware[f"A{i+1}"].top(), push_out = 20)
         
     diluent_pipette.return_tip()
-    for i, w in enumerate(dst_labware.wells()[:TRIALS]):
-        _run_trial(w, use_lld=bool((i % 2) == 0))  # switch using LLD every-other trial
+    if not baseline:
+        for i, w in enumerate(dst_labware.wells()[:TRIALS]):
+            _run_trial(w, use_lld=bool((i % 2) == 0))  # switch using LLD every-other trial
