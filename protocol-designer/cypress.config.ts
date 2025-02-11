e65af76821421dd3ee93b93df0091744d2fc0ac5
Sara Kowalski <sara.kowalski@opentrons.com>
2025-02-11 14:26:15 -0500

    more lint fixes

--- protocol-designer/cypress.config.ts
+++ protocol-designer/cypress.config.ts
@@ -1,15 +1,10 @@
-const { defineConfig } = require('cypress')
+import { defineConfig } from 'cypress'
 
 module.exports = defineConfig({
   video: false,
   viewportWidth: 1440,
   viewportHeight: 900,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     baseUrl: 'http://localhost:5178',
   },
 })
