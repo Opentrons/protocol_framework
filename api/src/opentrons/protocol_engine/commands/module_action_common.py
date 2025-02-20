bf2079a93118b22c1007a6afb2112d04227d4b92
tamarzanzouri <tamar.zanzouri@opentrons.com>
2025-02-20 12:39:17 -0500

    added a defined error for STACKER_SHUTTLE_EMPTY

---
+++ api/src/opentrons/protocol_engine/commands/module_action_common.py
@@ -0,0 +1,23 @@
+"""Common movement base models."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal, Any
+
+from opentrons_shared_data.errors import ErrorCodes
+from ..errors import ErrorOccurrence
+
+
+if TYPE_CHECKING:
+    from ..execution.movement import MovementHandler
+    from ..resources.model_utils import ModelUtils
+
+
+class StackerShuttleEmptyError(ErrorOccurrence):
+    """Returned when the machine detects that the stacker shuttle is empty."""
+
+    isDefined: bool = True
+    errorType: Literal["stackerShuttleEmpty"] = "stackerShuttleEmpty"
+
+    errorCode: str = ErrorCodes.STACKER_SHUTTLE_EMPTY.value.code
+    detail: str = ErrorCodes.STACKER_SHUTTLE_EMPTY.value.detail
