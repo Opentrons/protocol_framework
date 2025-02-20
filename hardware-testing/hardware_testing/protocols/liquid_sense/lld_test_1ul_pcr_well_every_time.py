4b3e9b11b615bc1ee32f7aa297b723fbb1c271e1
Andy Sigler <andrewsigler1@gmail.com>
2025-02-20 11:38:42 -0500

    load source PCR labware as empty

--- hardware-testing/hardware_testing/protocols/liquid_sense/lld_test_1ul_pcr_well_every_time.py
+++ hardware-testing/hardware_testing/protocols/liquid_sense/lld_test_1ul_pcr_well_every_time.py
@@ -163,6 +163,7 @@ def run(ctx: ProtocolContext) -> None:
     src_labware = ctx.load_labware(
         SRC_LABWARE, location=SLOTS["src"], version=get_latest_version(SRC_LABWARE)
     )
+    src_labware.load_empty(src_labware.wells())
 
     # LOAD stack of plates and lids, lid on bottom plate on top
     lids_and_plates = [ctx.load_labware("plate_lid", SLOTS["lids"])]
