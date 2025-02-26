7f60465422d85b954e1afc2b83faf0840dd75f5d
ncdiehl11 <ndiehl@opentrons.com>
2025-02-26 14:39:54 -0500

    refine util

--- protocol-designer/src/steplist/utils/getPreviousMoveToAddressableAreaCommand.ts
+++ protocol-designer/src/steplist/utils/getPreviousMoveToAddressableAreaCommand.ts
@@ -9,15 +9,13 @@ import type { CommandCreatorResult } from '@opentrons/step-generation'
 export const getPreviousMoveToAddressableAreaCommand = (
   frame: CommandCreatorResult
 ): MoveToAddressableAreaCreateCommand | null => {
-  let prevMoveToAddressableAreaCommand: MoveToAddressableAreaCreateCommand | null = null
   if ('commands' in frame) {
     for (let i = frame.commands.length - 1; i >= 0; i--) {
       const command = frame.commands[i]
       if (command.commandType === 'moveToAddressableArea') {
-        prevMoveToAddressableAreaCommand = command
-        break
+        return command
       }
     }
   }
-  return prevMoveToAddressableAreaCommand
+  return null
 }
