a24f8630251595910e67ae8d8c980a6f3f422042
Seth Foster <seth@opentrons.com>
2025-02-28 11:34:41 -0500

    app: load labware info from stacker commands
    slightly nicer

--- components/src/organisms/CommandText/useCommandTextString/utils/getLabwareDefinitionsFromCommands.ts
+++ components/src/organisms/CommandText/useCommandTextString/utils/getLabwareDefinitionsFromCommands.ts
@@ -6,17 +6,49 @@ import type { LabwareDefinition2, RunTimeCommand } from '@opentrons/shared-data'
 export function getLabwareDefinitionsFromCommands(
   commands: RunTimeCommand[]
 ): LabwareDefinition2[] {
-  return commands.reduce<LabwareDefinition2[]>((acc, command) => {
-    const isLoadingNewDef =
-      (command.commandType === 'loadLabware' ||
-        command.commandType === 'loadLid') &&
-      !acc.some(
-        def =>
-          command.result?.definition != null &&
-          getLabwareDefURI(def) === getLabwareDefURI(command.result?.definition)
+  return commands.reduce<LabwareDefinition2[]>(
+    (acc, command) => [
+      ...getLabwareDefinitionFromCommand(command, acc),
+      ...acc,
+    ],
+    []
+  )
+}
+
+function getNewLabwareDefinitions(
+  newDefinitions: Array<LabwareDefinition2 | null | undefined>,
+  knownDefinitions: LabwareDefinition2[]
+): LabwareDefinition2[] {
+  return newDefinitions.filter(
+    (maybeNewDef): maybeNewDef is LabwareDefinition2 =>
+      maybeNewDef != null &&
+      !knownDefinitions.some(
+        knownDef => getLabwareDefURI(knownDef) === getLabwareDefURI(maybeNewDef)
       )
-    return isLoadingNewDef && command.result?.definition != null
-      ? [...acc, command.result?.definition]
-      : acc
-  }, [])
+  )
+}
+
+const isLoadCommand = (
+  commandType: string
+): commandType is 'loadLabware' | 'loadLid' =>
+  ['loadLabware', 'loadLid'].includes(commandType)
+
+function getLabwareDefinitionFromCommand(
+  command: RunTimeCommand,
+  known: LabwareDefinition2[]
+): LabwareDefinition2[] {
+  if (isLoadCommand(command.commandType)) {
+    return getNewLabwareDefinitions([command.result?.definition], known)
+  }
+  if (command.commandType === 'flexStacker/setStoredLabware') {
+    return getNewLabwareDefinitions(
+      [
+        command.result?.primaryLabwareDefinition,
+        command.result?.adapterLabwareDefinition,
+        command.result?.lidLabwareDefinition,
+      ],
+      known
+    )
+  }
+  return known
 }
