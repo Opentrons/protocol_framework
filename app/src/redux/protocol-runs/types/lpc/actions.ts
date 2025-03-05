91e14bf499cf18bf3c987028b4d4d7ab0f3ffbf3
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-03-05 14:32:56 -0500

    refactor(app): update LPC types to better delineate between default and location-specific offsets

---
+++ app/src/redux/protocol-runs/types/lpc/actions.ts
@@ -0,0 +1,77 @@
+import type {
+  LPCStep,
+  LPCWizardState,
+  OffsetLocationDetails,
+  PositionParams,
+} from '/app/redux/protocol-runs/types/lpc/lpc'
+
+type LabwareURI = string
+
+export interface StartLPCAction {
+  type: 'START_LPC'
+  payload: { runId: string; state: LPCWizardState }
+}
+
+export interface FinishLPCAction {
+  type: 'FINISH_LPC'
+  payload: { runId: string }
+}
+
+export interface ProceedStepAction {
+  type: 'PROCEED_STEP'
+  payload: { runId: string; toStep?: LPCStep }
+}
+
+export interface GoBackStepAction {
+  type: 'GO_BACK_LAST_STEP'
+  payload: { runId: string }
+}
+
+export interface SelectedLabwareNameAction {
+  type: 'SET_SELECTED_LABWARE_NAME'
+  payload: {
+    runId: string
+    labwareUri: LabwareURI
+  }
+}
+
+export interface SelectedLabwareAction {
+  type: 'SET_SELECTED_LABWARE'
+  payload: {
+    runId: string
+    labwareUri: LabwareURI
+    location: OffsetLocationDetails | null
+  }
+}
+
+export interface ClearSelectedLabwareAction {
+  type: 'CLEAR_SELECTED_LABWARE'
+  payload: { runId: string }
+}
+
+export interface InitialPositionAction {
+  type: 'SET_INITIAL_POSITION'
+  payload: PositionParams & { runId: string }
+}
+
+export interface FinalPositionAction {
+  type: 'SET_FINAL_POSITION'
+  payload: PositionParams & { runId: string }
+}
+
+export interface ApplyOffsetAction {
+  type: 'APPLY_OFFSET'
+  payload: { runId: string; labwareUri: LabwareURI }
+}
+
+export type LPCWizardAction =
+  | StartLPCAction
+  | FinishLPCAction
+  | SelectedLabwareNameAction
+  | SelectedLabwareAction
+  | ClearSelectedLabwareAction
+  | InitialPositionAction
+  | FinalPositionAction
+  | ApplyOffsetAction
+  | ProceedStepAction
+  | GoBackStepAction
