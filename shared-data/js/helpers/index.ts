709688cf4332f3b488cc091aebbea0ccd1dce31a
Max Marrone <max@opentrons.com>
2025-03-05 16:10:11 -0500

    refactor(shared-data): Enumerate standard labware by globbing instead of hard-coding (#17631)

--- shared-data/js/helpers/index.ts
+++ shared-data/js/helpers/index.ts
@@ -6,6 +6,7 @@ import standardFlexDeckDef from '../../deck/definitions/5/ot3_standard.json'
 import type {
   DeckDefinition,
   LabwareDefinition2,
+  LabwareDefinition3,
   ModuleModel,
   RobotType,
   ThermalAdapterName,
@@ -44,7 +45,9 @@ export * from './sortRunTimeParameters'
 export const getLabwareDefIsStandard = (def: LabwareDefinition2): boolean =>
   def?.namespace === OPENTRONS_LABWARE_NAMESPACE
 
-export const getLabwareDefURI = (def: LabwareDefinition2): string =>
+export const getLabwareDefURI = (
+  def: LabwareDefinition2 | LabwareDefinition3
+): string =>
   constructLabwareDefURI(
     def.namespace,
     def.parameters.loadName,
