e22b4c3ba1d130b8441efe38a7bd5f35c02e5c69
ncdiehl11 <ndiehl@opentrons.com>
2025-03-05 13:13:57 -0500

    address feeback

--- shared-data/js/helpers/__tests__/linearInterpolate.test.ts
+++ shared-data/js/helpers/__tests__/linearInterpolate.test.ts
@@ -20,6 +20,17 @@ describe('linearInterpolate', () => {
     ])
     expect(result).toEqual(20)
   })
+  it('returns `left` value if target < first interpolationPoint value and `left` is defined', () => {
+    const result = linearInterpolate(
+      4,
+      [
+        [5, 20],
+        [10, 100],
+      ],
+      1
+    )
+    expect(result).toEqual(1)
+  })
   it('returns last point value if target > last interpolationPoint value', () => {
     const result = linearInterpolate(12, [
       [5, 20],
@@ -34,20 +45,44 @@ describe('linearInterpolate', () => {
     ])
     expect(result).toEqual(100)
   })
-  it('correctly linearly linearInterpolates if between two interpolation points', () => {
-    const result = linearInterpolate(20, [
-      [5, 20],
-      [10, 100],
-      [50, 120],
-    ])
-    expect(result).toEqual(105)
+  it('returns `right` value if target > last interpolationPoint value and `right` is defined', () => {
+    const result = linearInterpolate(
+      11,
+      [
+        [5, 20],
+        [10, 100],
+      ],
+      null,
+      1000
+    )
+    expect(result).toEqual(1000)
   })
-  it('correctly linearly linearInterpolates if between two interpolation points', () => {
+  it('correctly interpolates if target matches an interpolation points', () => {
     const result = linearInterpolate(10, [
       [5, 20],
       [10, 100],
       [50, 120],
     ])
     expect(result).toEqual(100)
+  })
+  it('correctly interpolates if between two interpolation points', () => {
+    const result = linearInterpolate(8, [
+      [5, 20],
+      [10, 100],
+      [50, 120],
+    ])
+    expect(result).toEqual(68)
+  })
+  it('correctly sorts and interpolates if between two interpolation points for unsorted input points', () => {
+    const result = linearInterpolate(8, [
+      [50, 120],
+      [5, 20],
+      [10, 100],
+    ])
+    expect(result).toEqual(68)
+  })
+  it('returns null if no points passed', () => {
+    const result = linearInterpolate(10, [])
+    expect(result).toEqual(null)
   })
 })
--- shared-data/js/helpers/linearInterpolate.ts
+++ shared-data/js/helpers/linearInterpolate.ts
@@ -1,22 +1,50 @@
+/**
+ * This function performs linear interpolation given a target x-value and an array of [x, y] known interpolation points.
+ * linearInterpolate will pre-sort interpolationPoints by x-value. If a value
+ *
+ * @param {number} target Target x-value for which to interpolate y-value.
+ * @param {Array<number, number>} interpolationPoints Array of [x, y] to use for interpolation.
+ * @param {number | null} [left] Value to use if target falls left of left-most interpolation point.
+ * @param {number | null} [right] Value to use if target falls right of right-most interpolation point.
+ *
+ * @returns {number | null} Interpolated y-value. Null if empty array of interpolation points passed.
+ */
 export const linearInterpolate = (
   target: number,
-  interpolationPoints: Array<[number, number]>
+  interpolationPoints: Array<[number, number]>,
+  left: number | null = null,
+  right: number | null = null
 ): number | null => {
   console.assert(
     interpolationPoints.length > 0,
-    'At least one bucket required for interpolation'
+    'At least one point required for interpolation'
   )
   if (interpolationPoints.length === 0) {
-    return interpolationPoints[0][1]
+    return null
   }
-  if (target < interpolationPoints[0][0]) {
-    return interpolationPoints[0][1]
+  const sortedInterpolationPoints = interpolationPoints.sort((a, b) => {
+    const xValueA = a[0]
+    const xValueB = b[0]
+    if (xValueA > xValueB) {
+      return 1
+    } else if (xValueA < xValueB) {
+      return -1
+    } else {
+      return 0
+    }
+  })
+  const lastPointIndex = sortedInterpolationPoints.length - 1
+  if (sortedInterpolationPoints.length === 0) {
+    return sortedInterpolationPoints[0][1]
   }
-  if (target > interpolationPoints[interpolationPoints.length - 1][0]) {
-    return interpolationPoints[interpolationPoints.length - 1][1]
+  if (target < sortedInterpolationPoints[0][0]) {
+    return left ?? sortedInterpolationPoints[0][1]
   }
-  for (let i = 0; i <= interpolationPoints.length - 1; i++) {
-    const bucket = interpolationPoints.slice(i, i + 2)
+  if (target > sortedInterpolationPoints[lastPointIndex][0]) {
+    return right ?? sortedInterpolationPoints[lastPointIndex][1]
+  }
+  for (let i = 0; i <= lastPointIndex; i++) {
+    const bucket = sortedInterpolationPoints.slice(i, i + 2)
     if (target >= bucket[0][0] && target <= bucket[1][0]) {
       const slope =
         (bucket[1][1] - bucket[0][1]) / (bucket[1][0] - bucket[0][0])
@@ -24,8 +52,7 @@ export const linearInterpolate = (
       return slope * target + yIntercept
     }
   }
-  console.warn(
-    'No interpolation point found. Make sure interpolation buckets are ordered correctly.'
-  )
+  // should never hit
+  console.warn('Unable to interpolate')
   return null
 }
