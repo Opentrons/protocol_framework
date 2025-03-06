33bbe9894c2d3c3e04a04827f88d3a4490d6aa69
Max Marrone <max@opentrons.com>
2025-03-06 17:53:15 -0500

    Switch api-client to POST.

--- api-client/src/labwareOffsets/getLabwareOffsets.ts
+++ api-client/src/labwareOffsets/getLabwareOffsets.ts
@@ -1,20 +1,10 @@
 import { GET, request } from '../request'
 
 import type { ResponsePromise } from '../request'
-import type {
-  HostConfig,
-  LabwareOffsetLocationSequenceComponent,
-} from '../types'
+import type { HostConfig } from '../types'
 import type { StoredLabwareOffset, MultiBodyMeta } from './types'
 
 export interface LabwareOffsetsSearchParams {
-  /** Filters are ORed together. Within a single filter, criteria are ANDed together. */
-  filters?: Array<{
-    id?: string
-    definitionUri?: string
-    locationSequence?: LabwareOffsetLocationSequenceComponent[]
-    mostRecentOnly?: boolean
-  }>
   cursor?: number
   pageLength?: number | 'unlimited'
 }
@@ -25,7 +15,7 @@ export interface LabwareOffsetsResponse {
 }
 
 /**
- * Get a filtered list of all the labware offsets currently stored on the robot.
+ * Get all the labware offsets currently stored on the robot.
  * Results are returned in order from oldest to newest.
  *
  * @param params - Optional filter parameters.
@@ -34,25 +24,11 @@ export function getLabwareOffsets(
   config: HostConfig,
   params: LabwareOffsetsSearchParams = {}
 ): ResponsePromise<LabwareOffsetsResponse> {
-  // If we naively give an array to axios's `params`:
-  //   {params: {ourArray: [1, 2]}}
-  // Then axios will serialize it in the URL like:
-  //   ?ourArray[]=1&?ourArray[]=2
-  // We don't want that. We want:
-  //   ?ourArray=[1,2]
-  // So, serialize it to a string ourselves before giving it to axios.
-  const stringifiedFilters =
-    params.filters === undefined ? undefined : JSON.stringify(params.filters)
-  const fixedParams = {
-    ...params,
-    filters: stringifiedFilters,
-  }
-
   return request<LabwareOffsetsResponse>(
     GET,
     '/labwareOffsets',
     null,
     config,
-    fixedParams
+    params
   )
 }
---
+++ api-client/src/labwareOffsets/searchLabwareOffsets.ts
@@ -0,0 +1,38 @@
+import { POST, request } from '../request'
+
+import type { ResponsePromise } from '../request'
+import type {
+  HostConfig,
+  LabwareOffsetLocationSequenceComponent,
+} from '../types'
+import type { StoredLabwareOffset } from './types'
+
+export interface SearchLabwareOffsetsData {
+  /** Filters are ORed together. Within a single filter, criteria are ANDed together. */
+  filters: Array<{
+    id?: string
+    definitionUri?: string
+    locationSequence?: LabwareOffsetLocationSequenceComponent[]
+    mostRecentOnly?: boolean
+  }>
+}
+
+export interface SearchLabwareOffsetsResponse {
+  data: StoredLabwareOffset[]
+}
+
+/**
+ * Search labware offsets by specific criteria.
+ *
+ * @param data - The criteria by which to search.
+ * @returns The matching labware offsets, oldest-to-newest.
+ */
+export function searchLabwareOffsets(
+  config: HostConfig,
+  data: SearchLabwareOffsetsData
+): ResponsePromise<SearchLabwareOffsetsResponse> {
+  return request<
+    SearchLabwareOffsetsResponse,
+    { data: SearchLabwareOffsetsData }
+  >(POST, '/labwareOffsets', { data }, config)
+}
