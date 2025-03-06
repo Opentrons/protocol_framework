82dfa59184214534caf56e498d94448e86ee60d1
David Chau <david.chau@opentrons.com>
2025-03-06 16:35:52 -0500

    Define a curryCommandCreator that suppresses Python.

--- step-generation/src/utils/index.ts
+++ step-generation/src/utils/index.ts
@@ -1,7 +1,7 @@
 import uuidv4 from 'uuid/v4'
 import { absorbanceReaderCollision } from './absorbanceReaderCollision'
 import { commandCreatorsTimeline } from './commandCreatorsTimeline'
-import { curryCommandCreator } from './curryCommandCreator'
+import { curryCommandCreator, curryWithoutPython } from './curryCommandCreator'
 import { reduceCommandCreators } from './reduceCommandCreators'
 import { modulePipetteCollision } from './modulePipetteCollision'
 import { thermocyclerPipetteCollision } from './thermocyclerPipetteCollision'
@@ -12,6 +12,7 @@ export {
   absorbanceReaderCollision,
   commandCreatorsTimeline,
   curryCommandCreator,
+  curryWithoutPython,
   reduceCommandCreators,
   modulePipetteCollision,
   thermocyclerPipetteCollision,
