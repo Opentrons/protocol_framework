d55b567227172f2cd1e4034d360d1e13329aa78c
Max Marrone <max@opentrons.com>
2025-03-07 12:34:49 -0500

    feat(robot-server): Support multiple filters in `GET /labwareOffsets` (#17672)

--- api-client/src/labwareOffsets/getLabwareOffsets.ts
+++ api-client/src/labwareOffsets/getLabwareOffsets.ts
@@ -3,14 +3,8 @@ import { GET, request } from '../request'
 import type { ResponsePromise } from '../request'
 import type { HostConfig } from '../types'
 import type { StoredLabwareOffset, MultiBodyMeta } from './types'
-import type { ModuleModel } from '@opentrons/shared-data'
 
 export interface LabwareOffsetsSearchParams {
-  id?: string
-  definitionUri?: string
-  locationSlotName?: string
-  locationModuleModel?: ModuleModel | null
-  locationDefinitionUri?: string | null
   cursor?: number
   pageLength?: number | 'unlimited'
 }
@@ -21,8 +15,8 @@ export interface LabwareOffsetsResponse {
 }
 
 /**
- * Get a filtered list of all the labware offsets currently stored on the robot.
- * Filters are ANDed together. Results are returned in order from oldest to newest.
+ * Get all the labware offsets currently stored on the robot.
+ * Results are returned in order from oldest to newest.
  *
  * @param params - Optional filter parameters.
  */
