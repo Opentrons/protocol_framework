11127705f6f9f08b989d5d017a6fd61698e3ef4d
Jamey Huffnagle <jamey.huffnagle@opentrons.com>
2025-03-07 16:38:08 -0500

    feat(app): Add LPC redesigned "remove probe" view on the ODD (#17627)
    Closes EXEC-1129

--- app/src/organisms/LabwarePositionCheck/steps/DetachProbe.tsx
+++ app/src/organisms/LabwarePositionCheck/steps/DetachProbe.tsx
@@ -1,12 +1,39 @@
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
+import styled from 'styled-components'
+import { useSelector } from 'react-redux'
+
+import { LegacyStyledText } from '@opentrons/components'
 
 import { LPCContentContainer } from '/app/organisms/LabwarePositionCheck/LPCContentContainer'
+import { selectActivePipetteChannelCount } from '/app/redux/protocol-runs'
+import { DescriptionContent, TwoColumn } from '/app/molecules/InterventionModal'
 
 import type { LPCWizardContentProps } from '/app/organisms/LabwarePositionCheck/types'
+
+import attachProbe1 from '/app/assets/videos/pipette-wizard-flows/Pipette_Attach_Probe_1.webm'
+import attachProbe8 from '/app/assets/videos/pipette-wizard-flows/Pipette_Attach_Probe_8.webm'
+import attachProbe96 from '/app/assets/videos/pipette-wizard-flows/Pipette_Attach_Probe_96.webm'
 
 export function DetachProbe(props: LPCWizardContentProps): JSX.Element {
   const { proceedStep, goBackLastStep } = props
   const { t } = useTranslation('labware_position_check')
+
+  const channelCount = useSelector(selectActivePipetteChannelCount(props.runId))
+
+  const probeVideo = (): string => {
+    switch (channelCount) {
+      case 1:
+        return attachProbe1
+      case 8:
+        return attachProbe8
+      case 96:
+        return attachProbe96
+      default: {
+        console.error('Unexpected channel count.')
+        return attachProbe1
+      }
+    }
+  }
 
   return (
     <LPCContentContainer
@@ -18,7 +45,30 @@ export function DetachProbe(props: LPCWizardContentProps): JSX.Element {
       }}
       onClickBack={goBackLastStep}
     >
-      <div>PLACEHOLDER DETACH PROBE</div>
+      <TwoColumn>
+        <DescriptionContent
+          headline={t('detach_probe')}
+          message={
+            <Trans
+              t={t}
+              i18nKey="store_probe"
+              components={{ block: <LegacyStyledText as="p" /> }}
+            />
+          }
+        />
+        <StyledVideo
+          autoPlay
+          loop
+          controls={false}
+          src={probeVideo()}
+          data-testid="probe-video"
+        />
+      </TwoColumn>
     </LPCContentContainer>
   )
 }
+
+const StyledVideo = styled.video`
+  height: 100%;
+  width: 100%;
+`
